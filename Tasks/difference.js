e366b6f6cd99047e986e239879216a0b75c72f36
Nikolas_2006 <kolia.biliavskiy@gmail.com>
2024-12-16 20:33:57 +0200

    - difference is done

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,19 +1,11 @@
 // Find a difference between two dictionaries
 
-const diff = (DIFF = function diff(object_1, object_2) {
-  for (attribute_name in object_1) {
-    object_1[attribute_name] = object_1[attribute_name];
+const diff = function (object1, object2) {
+  for (key in object2) {
+    delete object1[key];
   }
-  {
-  }
-  if (object_1 in object_2) return false;
-  for (attribute_name in object_2) {
-    object_1[attribute_name] = object_2[attribute_name];
-    delete object_1[attribute_name];
-  }
-  {
-  }
-  return object_1;
-});
+
+  return object1;
+};
 
 module.exports = diff;
