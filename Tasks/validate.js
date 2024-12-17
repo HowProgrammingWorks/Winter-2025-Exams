7e0bfcf951a13c196a7a393ec59a1f5289760d5c
Notna17 <tonketi2007@gmail.com>
2024-12-17 22:22:49 +0200

    Видалив непотрібний код

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -6,17 +6,12 @@
 const alphabet = ' abcdabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 const isValidate = (name) => {
-  if (!name) return false;
-  if (name === '') return false;
-  if (typeof name !== 'string') return false;
-  if (name.length === 0) return false;
-  if (!name.includes(' ')) return false;
-  {
+  if (typeof name !== 'string') return false
+  if (!name.includes(' ')) return false
     for (let char of name) {
       if (!alphabet.includes(char)) return false
     }
-    return true;
-  }
+    return true
 };
 
 module.exports = isValidate;
