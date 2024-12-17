8cebc90b40f0c99c65c76d71a2bfe1bbff31ce52
Notna17 <tonketi2007@gmail.com>
2024-12-17 21:57:14 +0200

    Використав const

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,17 @@
 // Refactor following solution
 // Get day number
 
-let dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+'use strict'
+
+const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 
 dayNumber = (day) => {
-  let i;
-  for (i = 0; i < dayNames.length; i++) {
+  for (const i = 0; i < dayNames.length; i++) {
     if (day.startsWith(dayNames[i].toLowerCase())) {
-      return i + 1;
+      return i + 1
     }
   }
-  return -1;
+  return -1
 };
 
 module.exports = dayNumber;
