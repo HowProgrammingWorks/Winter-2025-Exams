84c13c09038597f4e9bd83c7a655e1ee86075ed6
Notna17 <tonketi2007@gmail.com>
2024-12-17 21:14:52 +0200

    Переписав код, щоб працював для будь яких типів

--- Tasks/types.js
+++ Tasks/types.js
@@ -4,8 +4,9 @@
 types = function (s) {
   answer = {};
   for (i of s) {
-    const t = typeof i;
-    answer[t]++;
+    const t = typeof i
+    if (!answer[t]) answer[t] == 0
+    answer[t]++
   }
   return answer;
 };
