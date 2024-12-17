68e1e370fbb01f9f2e5f10dc648a8454431ae238
Notna17 <tonketi2007@gmail.com>
2024-12-17 20:47:42 +0200

    Стер лишнє

--- Tasks/types.js
+++ Tasks/types.js
@@ -2,18 +2,12 @@
 // Count types in an array
 
 types = function (s) {
-  answer = {
-    number: 0,
-    string: 0,
-    boolean: 0,
-  };
+  answer = {};
   for (i of s) {
     const t = typeof i;
     answer[t]++;
   }
-  s.push('string');
   return answer;
-  s.length;
 };
 
 module.exports = types;
