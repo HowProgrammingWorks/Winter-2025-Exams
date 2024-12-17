74d83fe9666d89d68ad2cda267f6a100836e56bc
Notna17 <tonketi2007@gmail.com>
2024-12-17 22:09:07 +0200

    Перейменував змінні

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,18 +1,20 @@
 // Refactor following solution
 // Validate person name
 
-const isValidate = (T) => {
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
+'use strict'
+
+const isValidate = (name) => {
+  if (!name) return false;
+  if (name === '') return false;
+  if (typeof name !== 'string') return false;
+  if (name.length === 0) return false;
+  if (!name.includes(' ')) return false;
   {
-    for (C of T) {
-      if (C === ' ') continue;
+    for (let char of name) {
+      if (char === ' ') continue;
       if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
+        char.toLowerCase().charCodeAt(0) >= 97 &&
+        char.toLowerCase().charCodeAt(0) <= 122
       ) {
       } else {
         return false;
