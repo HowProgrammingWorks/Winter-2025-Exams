d9e76f8e01ec32c976c52659b79be7d25095ea1c
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-18 23:52:57 +0200

    Fix mutation of incoming parameters

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,16 +1,17 @@
 // Copy only listed values from dict
 
-// Step 4
-// Change forEach method
+// Step 5
+// Fix mutation of incoming parameters
 
 'use strict';
 
 const only = (dict, ...only) => {
+  const copiedDict = {};
   const keys = Object.keys(dict);
   for (const key of keys) {
-    if (!only.includes(key)) delete dict[key];
+    if (only.includes(key)) copiedDict[key] = dict[key];
   }
-  return dict;
+  return copiedDict;
 };
 
 module.exports = only;
