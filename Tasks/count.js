6aad70d1ceff87330e0115674df1cd3441155a22
Liza-Khube <elisekhu@gmail.com>
2024-12-18 20:53:47 +0200

    Remove empty blocks

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,15 +5,12 @@
 
 count = (obj) => {
   sum = 0;
-  {
-  }
   keys = Object.keys(obj);
   keys.forEach((key) => {
     [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
