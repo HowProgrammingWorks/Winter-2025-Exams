a87b63188a3d6339ec466c5802ce6b0a6a75cad6
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-19 00:03:27 +0200

    Add filter method and remove if block in a loop

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,16 +1,15 @@
 // Copy only listed values from dict
 
-// Step 5
-// Fix mutation of incoming parameters
+// Step 6
+// Add Array.filter() method
+// Remove if block in a loop
 
 'use strict';
 
 const only = (dict, ...only) => {
   const copiedDict = {};
-  const keys = Object.keys(dict);
-  for (const key of keys) {
-    if (only.includes(key)) copiedDict[key] = dict[key];
-  }
+  const keys = Object.keys(dict).filter((key) => only.includes(key));
+  for (const key of keys) copiedDict[key] = dict[key];
   return copiedDict;
 };
 
