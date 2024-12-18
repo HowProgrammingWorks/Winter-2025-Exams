39ec8c0e2d4ac26f542190ec79bdf2743c30634d
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-18 23:26:36 +0200

    Add 'use strict' and remove useless lines

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,8 +1,12 @@
-// Refactor following solution
 // Copy only listed values from dict
 
+// Step1
+// Add 'use strict';
+// Remove useless lines
+
+'use strict';
+
 only = (W, ...only) => {
-  [];
   X = Object.keys(W, 'a', 'b', 'c');
   X.forEach((Z) => {
     if (only.includes(Z)) {
@@ -10,7 +14,6 @@ only = (W, ...only) => {
       delete W[Z];
     }
   }, 99);
-  [].sort();
   return W;
 };
 
