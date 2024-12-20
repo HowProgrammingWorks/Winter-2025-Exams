8019f45a2d5bc6037d3300c127d087e0582638ef
Nikolas_2006 <kolia.biliavskiy@gmail.com>
2024-12-20 19:54:56 +0200

    -duplicate is done

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,14 +1,11 @@
 // Return an array without duplicates
 
-duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+duplicate = (value, n) => {
+  const res = [];
+  for (let i = 0; i < n; i++) {
+    res.push(value);
   }
+  return res;
 };
 
 module.exports = duplicate;
