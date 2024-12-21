53bca3ce0ce453de581d63f8c91790be84077977
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-21 21:45:59 +0200

    Change loop with a reduce method

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,16 +1,13 @@
 // Find an intersection of two dictionaries
 
-// Step 5
-// Add filter method
+// Step 6
+// Change loop with a reduce method
 
 'use strict';
 
 const intersection = (obj1, obj2) => {
-  const inter = {};
-  const keys = Object.keys(obj1);
-  const filteredKeys = keys.filter((key) => obj1[key] === obj2[key]);
-  for (const key of filteredKeys) inter[key] = obj1[key];
-  return inter;
+  const keys = Object.keys(obj1).filter((key) => obj1[key] === obj2[key]);
+  return keys.reduce((acc, key) => ({ ...acc, [key]: obj1[key] }), {});
 };
 
 module.exports = intersection;
