4668b5c7a16ade7ad0c1af13055f4f33d1d5429f
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-21 20:43:52 +0200

    Add const and change var's namings

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,21 +1,21 @@
 // Find an intersection of two dictionaries
 
-// Step 1
-// Add 'use strict';
-// Remove empty blocks
+// Step 2
+// Add const
+// Change variables' names
 
 'use strict';
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
-  for (attribute_name of first_keys) {
-    if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
+const intersection = function intersection(obj1, obj2) {
+  const keys = Object.keys(obj1);
+  for (const key of keys) {
+    if (obj1[key] === obj2[key]) {
+      obj2[key] = obj1[key];
     } else {
-      delete object_1[attribute_name];
+      delete obj1[key];
     }
   }
-  return object_1;
+  return obj1;
 };
 
 module.exports = intersection;
