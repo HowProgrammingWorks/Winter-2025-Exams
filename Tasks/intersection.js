5d816d8565a0e41270ed1d751d9871e55ede3106
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-21 21:32:27 +0200

    Add filter method

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,16 +1,15 @@
 // Find an intersection of two dictionaries
 
-// Step 4
-// Remove delete operator
+// Step 5
+// Add filter method
 
 'use strict';
 
 const intersection = (obj1, obj2) => {
   const inter = {};
   const keys = Object.keys(obj1);
-  for (const key of keys) {
-    if (obj1[key] === obj2[key]) inter[key] = obj1[key];
-  }
+  const filteredKeys = keys.filter((key) => obj1[key] === obj2[key]);
+  for (const key of filteredKeys) inter[key] = obj1[key];
   return inter;
 };
 
