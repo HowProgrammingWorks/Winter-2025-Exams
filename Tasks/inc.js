a3d3b65debed12b0bc75524e1e44495e8a269bbf
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-22 12:50:04 +0200

    Add 'use strict' and 'const'

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,8 +1,13 @@
-// Refactor following solution
 // Increment all numbers in dictionary
 
-let inc_numbers = (format_complete, ...rest_variables) => {
-  for (delete_file in format_complete) {
+// Step 1
+// Add 'use strict'
+// Add `const`
+
+'use strict';
+
+const inc_numbers = (format_complete, ...rest_variables) => {
+  for (const delete_file in format_complete) {
     if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
       format_complete[delete_file] = format_complete[delete_file] + 1;
     }
