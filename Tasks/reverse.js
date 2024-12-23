ede86062ffe28b034a87041f34a98a54570c8cd9
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-23 20:45:44 +0200

    refactor (reverse.js): replace 'forEach' with 'for of'

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,14 +1,15 @@
 'use strict';
 
-// Step6: Avoid changing the original object in function
+// Step7: Replace 'forEach' with 'for'
+// 'for' is faster than 'forEach', has more appropriate semantics in this case
 
 const reverse = (data) => {
   const result = {};
   const keys = Object.keys(data);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = data[key];
     result[value] = key;
-  });
+  };
   return result;
 };
 
