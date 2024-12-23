f5893613991031dbd1bf410e2148abcfef27a147
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-23 21:20:24 +0200

    refactor (find.js): replace 'for in' with 'for of'

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,14 +1,14 @@
 'use strict';
 
-// Step5: Make an arrow function return a value
+// Step6: Replace 'for in' with 'for of'
+// Simplify code inside if by deleting block
 
 const find = (object, ...values) => {
   let result;
+  const keys = Object.keys(object);
   const value = values[0];
-  for (const key in object) {
-    if (object[key] === value) {
-      result = key;
-    }
+  for (const key of keys) {
+    if (object[key] === value) result = key;
   }
   return result;
 };
