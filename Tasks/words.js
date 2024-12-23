4a37bc42a236a28c37a89baaf06ba2c6e554c633
Liza-Khube <elisekhu@gmail.com>
2024-12-23 21:16:39 +0200

    Add 'const' and 'let'

--- Tasks/words.js
+++ Tasks/words.js
@@ -3,12 +3,12 @@
 
 "use strict";
 
-Words = function (s) {
-  number_of_words_in_s = 0;
-  flag = false;
-  for (c of s) {
+const Words = function (s) {
+  let number_of_words_in_s = 0;
+  let flag = false;
+  for (const c of s) {
     if (!flag) {
-      if (c === ' ') {
+      if (c === " ") {
         if (flag === true) {
           flag = false;
         } else {
@@ -23,7 +23,7 @@ Words = function (s) {
         number_of_words_in_s++;
       }
     } else {
-      if (c === ' ') {
+      if (c === " ") {
         if (flag === true) {
           flag = false;
         } else {
