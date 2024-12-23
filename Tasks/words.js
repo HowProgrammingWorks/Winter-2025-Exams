b4e337f93c1bc610d30b7ac48023825b54cc1659
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-23 17:13:50 +0200

    style (words.js): replace nested 'else-if' with 'else if'

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,7 +1,6 @@
 'use strict';
 
-// Step6: Change conditions for better clarity
-// Prefer more general conditions first to process more efficiently
+// Step7: Replace nested 'else-if' with 'else if'
 
 const words = (str) => {
   let wordsNumber = 0;
@@ -9,11 +8,9 @@ const words = (str) => {
   for (const char of str) {
     if (char === ' ') {
       isInsideWord = false;
-    } else {
-      if (!isInsideWord) {
+    } else if (!isInsideWord) {
         isInsideWord = true;
         wordsNumber++;
-      }
     }
   }
   return wordsNumber;
