f7d39aeaba537eeafff62514897edd9aca2abfd9
Liza-Khube <elisekhu@gmail.com>
2024-12-23 23:27:29 +0200

    Replace if-else statments with array of words

--- Tasks/words.js
+++ Tasks/words.js
@@ -5,22 +5,10 @@
 
 const counterOfWords = (str) => {
   let numberOfWords = 0;
-  let flag = false;
-  for (const word of str) {
-    if (!flag) {
-      if (word === " ") {
-        flag = false;
-      } else {
-        flag = true;
-        numberOfWords++;
-      };
-    } else {
-      if (word === " ") {
-        flag = false;
-      } else {
-        flag = true;
-      };
-    };
+  const arrayOfWords = str.split(" ");
+  for (const word of arrayOfWords) {
+    if (word === "") continue 
+    numberOfWords++;
   };
   return numberOfWords;
 };
