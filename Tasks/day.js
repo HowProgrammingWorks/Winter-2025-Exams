91bdd55c8fd911d359ca449dd71cd0e9083af757
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-23 09:46:08 +0200

    Change loop type

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,17 +1,14 @@
 // Get day number
 
-// Step 2
-// Change varable's case
-// Fix double `return`
+// Step 3
+// Change `for..` to a `for..of` loop
 
 const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (str) => {
   let result = -1;
-  for (let i = 0; i < DAYS.length; i++) {
-    if (str.startsWith(DAYS[i].toLowerCase())) {
-      result = i + 1;
-    }
+  for (const day of DAYS) {
+    if (str.startsWith(day.toLowerCase())) result = DAYS.indexOf(day) + 1;
   }
   return result;
 };
