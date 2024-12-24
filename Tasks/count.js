553fc9acd8a6cd63ab0d5355e6358d5043633e5a
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-24 19:02:35 +0200

    refactor (count.js): delete unused blocks of code

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,19 +1,14 @@
 'use strict';
 
-// Step3: Improve naming of identifiers
-// Prefer camelCase
+// Step4: Get rid of 'dead code' and unreachable code
 
 const countValues = (object) => {
   let sum = 0;
-  {
-  }
   const keys = Object.keys(object);
   keys.forEach((key) => {
-    [];
     const value = object[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
