7cb7f689aa77d18cff29d8205a73b65d6f783cb1
Nikolas_2006 <kolia.biliavskiy@gmail.com>
2024-12-24 23:28:01 +0200

    - sum is done

--- Tasks/password.js
+++ Tasks/password.js
@@ -4,7 +4,7 @@ let GeneratePassword = (alphabet, passwordLength) => {
   const maxIndex = alphabet.length;
   let password = '';
   for (let i = 0; i < passwordLength; i++) {
-    randomIndex = Math.floor(Math.random() * maxIndex);
+    const randomIndex = Math.floor(Math.random() * maxIndex);
     password = password + alphabet[randomIndex];
   }
   return password;
