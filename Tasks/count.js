bec8885e8b6271e2b2fc23e0e8835de8c9a281d4
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-24 19:16:35 +0200

    refactor (count.js): rewrite code into functional style

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// Step6: Use Object.values() directly instead of Object.keys()
+// Step7(optional): Rewrite code into functional style for better readability:
+// Use methods like reduce, filter and others
 
-const countValues = (object) => {
-  let sum = 0;
-  const values = Object.values(object);
-  for (const value of values) {
-    if (typeof value === 'number') sum += value;
-  };
-  return sum;
-};
+const countValues = (object) =>
+  Object.values(object)
+    .filter((value) => typeof value === 'number')
+    .reduce((sum, value) => sum + value, 0);
 
 module.exports = countValues;
