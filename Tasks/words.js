1f27fc962bd0d50ebdfcfd819164bd84d5d6c593
StudentPP1 <mishamakytonin@gmail.com>
2024-12-25 21:39:41 +0200

    4. words.js: changing to arrow function & add 'use strict'

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,12 +1,14 @@
 // Refactor following solution
 // Count words in a string
 
-Words = function (s) {
+"use strict"
+
+const words = (s) => {
   number_of_words_in_s = 0;
   flag = false;
   for (c of s) {
     if (!flag) {
-      if (c === ' ') {
+      if (c === " ") {
         if (flag === true) {
           flag = false;
         } else {
@@ -21,7 +23,7 @@ Words = function (s) {
         number_of_words_in_s++;
       }
     } else {
-      if (c === ' ') {
+      if (c === " ") {
         if (flag === true) {
           flag = false;
         } else {
@@ -39,4 +41,4 @@ Words = function (s) {
   return number_of_words_in_s;
 };
 
-module.exports = Words;
+module.exports = words;
