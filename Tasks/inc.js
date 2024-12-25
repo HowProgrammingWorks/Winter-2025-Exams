8114794b95e9baeaa1588002167e35c74022110c
StudentPP1 <mishamakytonin@gmail.com>
2024-12-25 21:09:50 +0200

    1. inc.js: formatting + 'use strict'

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,9 +1,10 @@
 // Refactor following solution
 // Increment all numbers in dictionary
+"use strict"
 
 let inc_numbers = (format_complete, ...rest_variables) => {
   for (delete_file in format_complete) {
-    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === 'N') {
+    if ((typeof format_complete[delete_file]).charAt(0).toUpperCase() === "N") {
       format_complete[delete_file] = format_complete[delete_file] + 1;
     }
   }
