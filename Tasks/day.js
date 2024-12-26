267f19803698fd1841f29922a52580b152af13e5
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-26 23:49:00 +0200

    Refactor: change 'DAYS' case to avoid a 'toLowerCase()' method

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,14 +2,12 @@
 
 'use strict';
 
-const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (str) => {
   let num = -1;
   for (let i = 0; i < DAYS.length; i++) {
-    if (str.startsWith(DAYS[i].toLowerCase())) {
-      num = ++i;
-    }
+    if (str.startsWith(DAYS[i])) num = ++i;
   }
   return num;
 };
