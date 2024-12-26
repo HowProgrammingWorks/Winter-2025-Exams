1fb4b912a0b70983de418e7aa74f2b88ebf637e5
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-27 00:02:52 +0200

    Refactor: change 'if-else' to a ternary operator

--- Tasks/day.js
+++ Tasks/day.js
@@ -6,11 +6,7 @@ const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (str) => {
   const findDay = DAYS.find((day) => str.startsWith(day));
-  if (findDay) {
-    return DAYS.indexOf(findDay) + 1;
-  } else {
-    return -1;
-  }
+  return findDay ? DAYS.indexOf(findDay) + 1 : -1;
 };
 
 module.exports = parseDay;
