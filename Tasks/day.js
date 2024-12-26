9e467ac8885a31634542aefcb913745fd002cd6f
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-27 00:00:42 +0200

    Refactor: change the loop to a 'find()' method and add 'if-else' operators

--- Tasks/day.js
+++ Tasks/day.js
@@ -5,11 +5,12 @@
 const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (str) => {
-  let num = -1;
-  for (let i = 0; i < DAYS.length; i++) {
-    if (str.startsWith(DAYS[i])) num = ++i;
+  const findDay = DAYS.find((day) => str.startsWith(day));
+  if (findDay) {
+    return DAYS.indexOf(findDay) + 1;
+  } else {
+    return -1;
   }
-  return num;
 };
 
 module.exports = parseDay;
