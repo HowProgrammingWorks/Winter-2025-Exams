5955c66d40d0752d23f10d71bc3cc5066f8cfd06
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-26 22:21:19 +0200

    Refactor: fix mutation of incoming parameters

--- Tasks/only.js
+++ Tasks/only.js
@@ -3,11 +3,12 @@
 'use strict';
 
 const only = (dict, ...only) => {
+  const onlyDict = {};
   const keys = Object.keys(dict);
   for (const key of keys) {
-    if (!only.includes(key)) delete dict[key];
+    if (only.includes(key)) onlyDict[key] = dict[key];
   }
-  return dict;
+  return onlyDict;
 };
 
 module.exports = only;
