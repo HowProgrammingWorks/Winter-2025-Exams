cb3063b82cc29a9b59732338f4fabfb730e85793
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-26 22:16:21 +0200

    Refactor: change 'forEach()' method to a loop

--- Tasks/only.js
+++ Tasks/only.js
@@ -4,11 +4,9 @@
 
 const only = (dict, ...only) => {
   const keys = Object.keys(dict);
-  keys.forEach((key) => {
-    if (!only.includes(key)) {
-      delete dict[key];
-    }
-  });
+  for (const key of keys) {
+    if (!only.includes(key)) delete dict[key];
+  }
   return dict;
 };
 
