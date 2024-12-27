a5179e92f46767269d73fbbd446879eceee36ef7
Ilya Sizonenko <sizonenkoilya0@gmail.com>
2024-12-27 21:38:25 +0200

    3)remove unneeded code

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,15 +1,10 @@
 // Refactor following solution
 // Generate random integer value in given range
 
-//Second step: format to camelCase
+//Third step: remove unneeded code, symplify function
 
 const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
-};
+  return min + Math.floor(Math.random() * (max - min + 1));
+}
 
 module.exports = random;
