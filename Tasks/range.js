df43fb61c492e4a771b1ca242a1e63d62800e91f
Ilya Sizonenko <sizonenkoilya0@gmail.com>
2024-12-27 22:00:49 +0200

    6)rename function

--- Tasks/range.js
+++ Tasks/range.js
@@ -1,14 +1,17 @@
 // Refactor following solution
 // Generate int array from given range
 
-//fifth step: simplify function. Add preliminary conditional, delete unnecessary if..else
-const _range = (from, to) => {
+//Sixth step: rename function, add empty lines for readability
+
+const range = (from, to) => {
   if (from > to) return [];
+
   const array = [];
   for (let i = from; i <= to; i++) {
       array.push(i);
   }
+
   return array;
 };
 
-module.exports = _range;
+module.exports = range;
