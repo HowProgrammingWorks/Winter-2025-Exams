3155ef6f0397c57313698444dfbcccfecdc9f587
Sofia_Belichenko <sofiabelicenko292@gmail.com>
2024-12-27 20:22:07 +0200

    password: step_1

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,14 +1,22 @@
-// Refactor following solution
 // Generate random password
 
-let GeneratePassword = (alphabet, length) => {
-  const MAX = alphabet.length;
-  key = '';
+// Step 1
+//
+// Add 'use strict';
+// Fix formatting
+// Prefer const
+// Improve naming
+
+'use strict'
+
+const generatePassword = (alphabet, length) => {
+  const maxLength = alphabet.length;
+  let key = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
+    const index = Math.floor(Math.random() * maxLength);
+    key = key + alphabet[index];
   }
   return key;
 };
 
-module.exports = GeneratePassword;
+module.exports = generatePassword;
