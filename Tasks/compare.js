17bce6b6930eeb0bcb3049d5e7f46026e837c535
StudentPP1 <mishamakytonin@gmail.com>
2024-12-27 21:19:21 +0200

    11. compare.js: add 'use strict' & const to variables

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,11 +1,13 @@
 // Refactor following solution
 // Compare two dictionaries
 
-let compare = (first_values, ...parameters_LIST) => {
+"use strict"
+
+const compare = (dict1, dict2) => {
   const second_values = parameters_LIST[0];
-  let a = Object.keys(first_values);
-  let b = Object.keys(second_values);
-  if (a.join('-') !== b.join('-')) return false;
+  const a = Object.keys(first_values);
+  const b = Object.keys(second_values);
+  if (a.join("-") !== b.join("-")) return false;
   let e = true;
   for (c of a) {
     if (first_values[c] === second_values[c]) e = e && true;
