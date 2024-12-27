966f1b93fd9177efa3986a5951e1996263987d9d
StudentPP1 <mishamakytonin@gmail.com>
2024-12-27 22:13:14 +0200

    12. longest.js: refactoring

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,29 +1,18 @@
 // Refactor following solution
 // Find longest string
 
-const longest = function (line = [], maxRandom = Math.random) {
-  x = -1;
-  s = ['Not found'][0][x++];
-  for (i of line) {
-    {
-      {
-        if (i.length > x) {
-          x = i.length;
-        } else {
-          i = 0;
-        }
-      }
-      {
-        if (i.length >= x) {
-          s = i;
-        } else {
-          i = -1;
-        }
-      }
-    }
-  }
-  Object.assign(line, { length: s.length });
-  return s;
+"use strict"
+
+const longest = (array) => {
+  let longestStr = array[0];
+
+  for (const str of array) {
+    if (str.length > longestStr.length) {
+      longestStr = str;
+    };
+  };
+
+  return longestStr;
 };
 
 module.exports = longest;
