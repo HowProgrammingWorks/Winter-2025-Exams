9f4cfc5d59307693a2135c2ab902b4b5afd15566
Ilya Sizonenko <sizonenkoilya0@gmail.com>
2024-12-27 15:48:26 +0200

    3)remove mutation

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,18 +1,18 @@
 // Refactor following solution
 // Filter array by type name
 
-//Second step: rename variables and format to camelCase
+//Third step: remove mutation of incoming parametrs. Replace "remove" with res array, change "if" conditon and return 
 
 const filter = (array, type) => {
-  const remove = [];
+  const res = [];
   for (const el of array) {
     const index = array.indexOf(el);
-    if (typeof array[index] !== type) {
-      remove.unshift(index);
+    if (typeof array[index] === type) {
+      res.push(el);
     }
   }
-  for (const el of remove) array.splice(el, 1);
-  return array;
+  for (const el of res) array.splice(el, 1);
+  return res;
 };
 
 module.exports = filter;
