6d0c5f5b150e72ad931ddd1666c05d4bd972d580
Sofia_Belichenko <sofiabelicenko292@gmail.com>
2024-12-27 20:20:10 +0200

    filter: step_1

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,16 +1,23 @@
-// Refactor following solution
 // Filter array by type name
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
-    if (typeof T[x] !== t) {
-      remove.unshift(x);
+// Step 1
+// Add 'use strict';
+// Fix formatting
+// Prefer const
+// Improve naming
+
+'use strict';
+
+const filter = (array, type) => {
+  let remove = [];
+  for (const element of array) {
+    const index = array.indexOf(element);
+    if (typeof array[index] !== type) {
+      remove.unshift(index);
     }
   }
-  for (x of remove) T.splice(x, 1);
-  return T;
+  for (const index of remove) array.splice(index, 1);
+  return array;
 };
 
-module.exports = Filter;
+module.exports = filter;
