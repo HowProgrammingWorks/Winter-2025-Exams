25d057503ee89ea289cb77478eefb28a5b777058
Sofia_Belichenko <sofiabelicenko292@gmail.com>
2024-12-27 20:21:20 +0200

    filter: step_4

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,18 +1,12 @@
 // Filter array by type name
 
-// Step 3
-// Don't mutate incoming parameters
+// Step 4
+// Changing the approach, now we're using a filter
 
 'use strict';
 
 const filter = (array, type) => {
-  let newArray = [];
-  for (const index in array) {
-    if (typeof array[index] === type) {
-      newArray.push(array[index]);
-    }
-  }
-  return newArray;
+  return array.slice().filter(item => typeof item === type);
 };
 
 module.exports = filter;
