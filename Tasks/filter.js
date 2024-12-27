712d45962eca1bdee07e41e915b99cfaeb1bd7ea
Sofia_Belichenko <sofiabelicenko292@gmail.com>
2024-12-27 20:20:57 +0200

    filter: step_3

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,21 +1,18 @@
 // Filter array by type name
 
-// Step 2
-// replace for...of to for...in
-// remove "const index = array.indexOf(element)"
-
+// Step 3
+// Don't mutate incoming parameters
 
 'use strict';
 
 const filter = (array, type) => {
-  let remove = [];
+  let newArray = [];
   for (const index in array) {
-    if (typeof array[index] !== type) {
-      remove.unshift(index);
+    if (typeof array[index] === type) {
+      newArray.push(array[index]);
     }
   }
-  for (const index of remove) array.splice(index, 1);
-  return array;
+  return newArray;
 };
 
 module.exports = filter;
