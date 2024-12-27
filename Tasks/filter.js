8784e5d7a5fd33905609d518bbdc3d32c1a3a0c0
Ilya Sizonenko <sizonenkoilya0@gmail.com>
2024-12-27 15:50:16 +0200

    4)remove unneeded code

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,17 +1,15 @@
 // Refactor following solution
 // Filter array by type name
 
-//Third step: remove mutation of incoming parametrs. Replace "remove" with res array, change "if" conditon and return 
+//Fourth step: remove unnecessary code 
 
 const filter = (array, type) => {
   const res = [];
   for (const el of array) {
-    const index = array.indexOf(el);
-    if (typeof array[index] === type) {
+    if (typeof el === type) {
       res.push(el);
     }
   }
-  for (const el of res) array.splice(el, 1);
   return res;
 };
 
