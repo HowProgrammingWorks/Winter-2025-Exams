b328dcec151f88df151883588fb53ae8711aee8f
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-28 09:58:08 +0200

    feat (schedule.js): await new promise to set interval

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -2,10 +2,11 @@
 
 // Implement following task
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
-//Step1: Implement task with 'for' loop to call function `n`-times
+//Step2: await new Promise to set interval between calls of function
 
 const schedule = async (func, n, interval) => {
   for (let i = 0; i < n; i++) {
+    await new Promise((resolve) => setTimeout(resolve, interval));
     func(i);
   }
 };
