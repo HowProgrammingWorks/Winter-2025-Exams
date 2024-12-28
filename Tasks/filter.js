95054493723bffd96ef3159b2a9d8ba5d8afaa41
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-28 23:23:31 +0200

    refactor (filter.js): rewrite code into functional style

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,15 +1,8 @@
 'use strict';
 
-// Step6: Simplify logic
+// Step7: Rewrite into functional style with the help of .filter
 
-const filterArray = (array, type) => {
-  const result = [];
-  for (const item of array) {
-    if (typeof item === type) {
-      result.push(item);
-    }
-  }
-  return result;
-};
+const filterArray = (array, type) =>
+  array.filter((item) => typeof item === type);
 
 module.exports = filterArray;
