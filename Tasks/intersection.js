8b65077a454f378dbd6b1a89e266ccbf4e4a9931
Notna17 <tonketi2007@gmail.com>
2024-12-28 13:22:35 +0200

    Видалив лишні дужки

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,25 +2,16 @@
 // Find an intersection of two dictionaries
 
 const intersection = (obj1, obj2) => {
-  keys1 = Object.keys(obj1);
-  {
-  }
+  keys1 = Object.keys(obj1)
   for (key of keys1) {
     if (obj1[key] === obj2[key]) {
-      obj2[key] = obj1[key];
-      {
-      }
+      obj2[key] = obj1[key]
     } else {
-      delete obj1[key];
-      {
-      }
-    }
-    {
+      delete obj1[key]
     }
   }
-  {
-  }
-  return obj1;
-};
+
+  return obj1
+}
 
 module.exports = intersection;
