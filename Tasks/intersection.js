c5c46c8f9fb053d069d210b93b30e3cce626660b
Notna17 <tonketi2007@gmail.com>
2024-12-28 13:26:31 +0200

    Переписав код так, щоб не змінював основні дані

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,16 +2,13 @@
 // Find an intersection of two dictionaries
 
 const intersection = (obj1, obj2) => {
+  let answer = {}
   keys1 = Object.keys(obj1)
   for (key of keys1) {
-    if (obj1[key] === obj2[key]) {
-      obj2[key] = obj1[key]
-    } else {
-      delete obj1[key]
-    }
+    if (obj1[key] === obj2[key]) answer[key] = obj1[key]
   }
 
-  return obj1
+  return answer
 }
 
 module.exports = intersection;
