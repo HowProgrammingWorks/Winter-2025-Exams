247a1d6b09eb39efa7c6e59b436fe7a1435e5341
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-28 22:59:35 +0200

    fix (filter.js): fix variables declaration

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,16 +1,18 @@
 'use strict';
 
-// Step1: Add 'use strict'
+// Step2: Prefer 'const' for declaration of never reassigned identifiers
+// Prefer 'let' if 'const' cannot be used
+// Never use 'var', it`s outdated
 
-Filter = (T, t) => {
-  remove = [];
-  for (C of T) {
-    x = T.indexOf(C);
+const Filter = (T, t) => {
+  const remove = [];
+  for (const C of T) {
+    const x = T.indexOf(C);
     if (typeof T[x] !== t) {
       remove.unshift(x);
     }
   }
-  for (x of remove) T.splice(x, 1);
+  for (const x of remove) T.splice(x, 1);
   return T;
 };
 
