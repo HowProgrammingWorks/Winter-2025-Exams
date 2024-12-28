9a18f3c4aef7a0a20a0f39c9dbae3e9c37df46d3
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-28 09:51:49 +0200

    feat (schedule.js): implement task with 'for' loop to call function `n`-times

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -2,7 +2,12 @@
 
 // Implement following task
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
+//Step1: Implement task with 'for' loop to call function `n`-times
 
-const schedule = async (func, n, interval) => {};
+const schedule = async (func, n, interval) => {
+  for (let i = 0; i < n; i++) {
+    func(i);
+  }
+};
 
 module.exports = schedule;
