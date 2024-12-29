7baa9818cfd8c11d982f2cdb9b193a3673a642f6
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-29 10:34:17 +0200

    fix (types.js): fix variables declaration

--- Tasks/types.js
+++ Tasks/types.js
@@ -1,14 +1,16 @@
 'use strict';
 
-// Step1: Add 'use strict'
+// Step2: Prefer 'const' for declaration of never reassigned identifiers
+// Prefer 'let' if 'const' cannot be used
+// Never use 'var', it`s outdated
 
-types_ = function (s) {
-  types_ = {
+const types_ = function (s) {
+  const types_ = {
     number: 0,
     string: 0,
     boolean: 0,
   };
-  for (i of s) {
+  for (const i of s) {
     const t = typeof i;
     types_[t]++;
   }
