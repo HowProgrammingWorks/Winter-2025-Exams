57a79c3c7af42db2f1fb8e559379e51f27fbda11
DimaBohdan <bohdan777.dmytro@gmail.com>
2024-12-29 14:21:40 +0200

    refactor (zip.js): add 'use strict'

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,5 +1,6 @@
-// Refactor following solution
-// Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
+'use strict';
+
+// Step1: Add 'use strict'
 
 const zip = function (a = [], b = []) {
   let i = 0;
