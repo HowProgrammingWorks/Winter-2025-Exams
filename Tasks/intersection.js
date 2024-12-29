ef5a3d633de2660621e57366d666e96aaf582fbc
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-12-29 14:08:34 +0200

    Refactor: change identifiers' case and simplify 'if' statement

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -2,16 +2,14 @@
 
 'use strict';
 
-const intersection = (object_1, object_2) => {
-  const first_keys = Object.keys(object_1);
-  for (const attribute_name of first_keys) {
-    if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
-    } else {
-      delete object_1[attribute_name];
+const intersection = (obj1, obj2) => {
+  const keys = Object.keys(obj1);
+  for (const key of keys) {
+    if (obj1[key] !== obj2[key]) {
+      delete obj1[key];
     }
   }
-  return object_1;
+  return obj1;
 };
 
 module.exports = intersection;
