55c4dbe5f89843e8b2447d58ecbab699109d6e4a
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 22:15:54 +0200

    count (step 4)

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,15 +1,12 @@
-// STEP 3:
-//add new const "values"
+// STEP 4:
+//Add ternary operator
+//Rename sum to acc
 
 const count = (obj) => {
     const values = Object.values(obj);
-    const res = values.reduce((sum, value) => {
-      if (typeof value === 'number') {
-        return sum + value;
-      }
-      return sum;
-    }, 0);
+    const res = values.reduce((acc, value) =>
+        typeof value === 'number' ? acc + value : acc, 0);
     return res;
-  };
-  
-  module.exports = count;
+};
+
+module.exports = count;
