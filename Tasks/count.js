992e45e6935e3dbfd14f206af48b1bf1b7d53e98
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 22:14:02 +0200

    count (step 1)

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,18 +1,15 @@
-// Refactor following solution
-// Sum all number values in dict
+// STEP 1:
+//Remove useless brackets
+//Add "const" and "let"
 
-count = (obj) => {
-  sum = 0;
-  {
-  }
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
-    [];
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
-  ({});
-  return sum;
+const count = (obj) => {
+    let sum = 0;
+    const keys = Object.keys(obj);
+    keys.forEach((key) => {
+        const value = obj[key];
+        if (typeof value === 'number') sum += value;
+    });
+    return sum;
 };
 
 module.exports = count;
