0e887a45c162248629af51e6d8da5e9140ad53eb
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 21:54:12 +0200

    compare (step 3)

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,6 +1,5 @@
-// STEP 2:
-//Do some renamings.
-//Remove "let"s with "const"s
+//STEP 3:
+//Remove cycle with "every" method
 
 const compare = (dict1, dict2) => {
     const keys1 = Object.keys(dict1);
@@ -8,12 +7,8 @@ const compare = (dict1, dict2) => {
     if (keys1.join('-') !== keys2.join('-')) {
         return false;
     };
-    for (const key of keys1) {
-        if (dict1[key] !== dict2[key]) {
-            return false;
-        }
-    }
-    return true;
+    const check = keys1.every((key) => dict1[key] === dict2[key]);
+    return check;
 };
 
 module.exports = compare;
