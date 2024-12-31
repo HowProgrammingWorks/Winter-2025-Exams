c5559a50ab17097ea3fe3fbb8a38fac736bbe0b7
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 22:05:29 +0200

    sample (step 2)

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,9 +1,12 @@
-//STEP 1:
-//Do some renaming
+//STEP 2:
+//Add new const "RandIndex"
+//We should return new const,not "array"
 
 const sample = (array) => {
-  array = array[Math.floor(Math.random() * array.length)];
-  return array;
+  const randIndex = Math.floor(Math.random() * array.length)
+  const randElement = array[randIndex];
+  return randElement;
 };
 
 module.exports = sample;
+
