de207fd0bc7444a54f2ef6df2b80930be47e97ba
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 21:53:28 +0200

    compare (step 1)

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,19 +1,19 @@
-// Refactor following solution
-// Compare two dictionaries
+// STEP 1:
+//Second parametr with rest operator complitely useless.
+//Remove "e" and everething that related to it,
+// change logic of values`es checking.
+//Use eslint.
 
-let compare = (first_values, ...parameters_LIST) => {
-  const second_values = parameters_LIST[0];
-  let a = Object.keys(first_values);
-  let b = Object.keys(second_values);
-  if (a.join('-') !== b.join('-')) return false;
-  let e = true;
-  for (c of a) {
-    if (first_values[c] === second_values[c]) e = e && true;
-    else {
-      e = e && false;
-    }
-  }
-  return e;
-};
+let compare = (first_values, second_values) => {
+    let a = Object.keys(first_values);
+    let b = Object.keys(second_values);
+    if (a.join('-') !== b.join('-')) return false;
+    for (c of a) {
+        if (first_values[c] !== second_values[c]) {
+            return false;
+        }
+    };
+    return true;
+}
 
 module.exports = compare;
