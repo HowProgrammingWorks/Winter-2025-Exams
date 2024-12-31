70dafe266e4a852f80a2efc7b5db12859ef39028
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 20:39:18 +0200

    words v2 (from scratch)

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,10 +1,19 @@
 // Count words in a string
+// version with flags
 
 const wordsCounter = (string) => {
-  const words = string.split(" ");
-  const filtered = words.filter((word) => word !== ""); //if string has double+ spaces
-  const count = filtered.length;
-  return count;
+  let counter = 0;
+  let flag = true;
+  for (const char of string) {
+    if (char === " ") {
+      flag = true;
+    }
+    else if (char !== " " && flag == 1) {
+      counter++;
+      flag = false;
+    }
+  }
+  return counter;
 };
 
 module.exports = wordsCounter;
