55817b93ca9b74857155672f249eb4d9db670a28
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 21:57:20 +0200

    month (step 1)

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,27 +1,30 @@
-// Refactor following solution
-// Get month number
+// STEP 1:
+//Do some renamings 
+//Remove "let"s with "const"s
+//Add new const scope
+//Use eslint
 
-Months = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'may',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'oct',
-  'nov',
-  'dec',
+const list = [
+    'jan',
+    'feb',
+    'mar',
+    'apr',
+    'may',
+    'jun',
+    'jul',
+    'aug',
+    'sep',
+    'oct',
+    'nov',
+    'dec',
 ];
 
-Month = (s) => {
-  l = Months.length;
-  for (let i = 0; i < l; i++) {
-    if (s.toLowerCase().startsWith(Months[i])) return i + 1;
-  }
-  return -1;
+const position = (month) => {
+    const scope = list.length;
+    for (let i = 0; i < scope; i++) {
+        if (month.toLowerCase().startsWith(list[i])) return i + 1;
+    }
+    return -1;
 };
 
-module.exports = Month;
+module.exports = position;
