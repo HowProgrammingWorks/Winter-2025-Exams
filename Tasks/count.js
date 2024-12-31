0cb5d5b7fd05d535581e75fdf99e382586c3c65e
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 22:15:11 +0200

    count (step 3)

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,16 +1,15 @@
-// STEP 2:
-//Remove .forEach with "Object.values(obj).reduce"
-//Add new const "res"
-////Remove useless parts
+// STEP 3:
+//add new const "values"
 
 const count = (obj) => {
-    const res = Object.values(obj).reduce((sum, value) => {
+    const values = Object.values(obj);
+    const res = values.reduce((sum, value) => {
       if (typeof value === 'number') {
         return sum + value;
       }
       return sum;
     }, 0);
-    return res
+    return res;
   };
   
   module.exports = count;
