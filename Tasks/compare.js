87503ef7f15dc8b6d769e303a990144baf8859a5
MihajloVat <vatolkinmihajlo@gmail.com>
2024-12-31 21:54:39 +0200

    compare (step 4)

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,14 +1,13 @@
-//STEP 3:
-//Remove cycle with "every" method
+//STEP 4:
+//Remove current keys`es checker with "some" method.
+//Add new const,use boolean`s logic,rename "check" to "check2".
 
 const compare = (dict1, dict2) => {
     const keys1 = Object.keys(dict1);
     const keys2 = Object.keys(dict2);
-    if (keys1.join('-') !== keys2.join('-')) {
-        return false;
-    };
-    const check = keys1.every((key) => dict1[key] === dict2[key]);
-    return check;
+    const check1 = keys1.every((key,index) => key === keys2[index]);
+    const check2 = keys1.every((key) => dict1[key] === dict2[key]);
+    return check1 && check2;
 };
 
 module.exports = compare;
