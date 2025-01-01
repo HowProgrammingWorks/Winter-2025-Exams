8d30e062020af1986afc6f55b10c36ff30f2f31a
Oleksandr Cherepov <sashacherepov55@gmail.com>
2025-01-01 12:51:51 +0200

    Refactor: remove 'if' block by adding extra variable

--- Tasks/random.js
+++ Tasks/random.js
@@ -3,11 +3,10 @@
 'use strict';
 
 const randNum = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    min = 0;
-  }
-  return min + Math.floor(Math.random() * (max - min + 1));
+  const range = max ? max - min : min;
+  return max
+    ? Math.floor(Math.random() * (range + 1)) + min
+    : Math.floor(Math.random() * (range + 1));
 };
 
 module.exports = randNum;
