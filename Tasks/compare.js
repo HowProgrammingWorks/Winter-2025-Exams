53f5e9855358b2b58e918dc42767000f23d8bd8a
Maksim Ostroginskiy <ostroginskiymaksim@gmail.com>
2025-01-02 17:24:17 +0200

    Optimization

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -4,19 +4,13 @@
 const compare = (obj1, obj2) => {
   const firstObjKeys = Object.keys(obj1);
   const secondObjKeys = Object.keys(obj2);
-  let result = true
 
 
-  if (firstObjKeys.join('-') !== secondObjKeys.join('-')) return false;
-  for (const key of firstObjKeys) {
-    if (obj1[key] === obj2[key]) result = result && true
-    else {
-      result = result && false
-    }
-  }
-
+  if(firstObjKeys.length !== secondObjKeys.length) return false
   
-  return result;
+  if(!firstObjKeys.every((key, index) => key === secondObjKeys[index])) return false
+
+  return firstObjKeys.every((key) => obj1[key] === obj2[key])
 };
 
 module.exports = compare;
