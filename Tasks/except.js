ba6927e6ea47b5b2cdeaed2ee3509e8fad893a03
DimaBohdan <bohdan777.dmytro@gmail.com>
2025-01-02 10:46:16 +0200

    refactor (except.js): improve functional style

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,14 +1,12 @@
 'use strict';
 
-// Step7(optional): Rewrite code into functional style for better readability:
-// Use methods like reduce, filter and others
+// Step8(optional): Improve functional style:
+// Use methods like fromEntries, filter and others
 
 const except = (inputObject, ...exceptedKeys) =>
-  Object.keys(inputObject)
-    .filter((key) => !exceptedKeys.includes(key))
-    .reduce((result, key) => {
-      result[key] = inputObject[key];
-      return result;
-    }, {});
+  Object.fromEntries(
+    Object.entries(inputObject)
+      .filter(([key]) => !exceptedKeys.includes(key)),
+  );
 
 module.exports = except;
