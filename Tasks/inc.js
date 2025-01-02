0d9f2b5c9949b574ccd93dbeca020906b37b63af
Liza-Khube <elisekhu@gmail.com>
2025-01-02 23:13:22 +0200

    Remove unused parameter

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -3,7 +3,7 @@
 
 "use strict";
 
-const incNumbers = (object, ...restValue) => {
+const incNumbers = (object) => {
   for (const key in object) {
     if ((typeof object[key]).charAt(0).toUpperCase() === "N") {
       object[key] = object[key] + 1;
