df713904fe1f51d7ee03021bb57ac523cb564e67
pc <lovekuchernazarlove@gmail.com>
2025-01-02 12:46:27 +0200

    Optimize key comparison by removing `join` method
    - Replaced `firstKeys.join('-') !== secondKeys.join('-')` with a length check.

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -7,7 +7,7 @@ const compareDictionaries = (firstDict, ...parametersList) => {
   const firstKeys = Object.keys(firstDict);
   const secondKeys = Object.keys(secondDict);
 
-  if (firstKeys.join('-') !== secondKeys.join('-')) return false;
+  if (firstKeys.length !== secondKeys.length) return false;
 
   for (const key of firstKeys) {
     if (firstDict[key] !== secondDict[key]) {
