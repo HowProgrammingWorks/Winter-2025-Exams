26aeb90f0fc4496cae8523a39639c79ff7a9fabe
Maksim Ostroginskiy <ostroginskiymaksim@gmail.com>
2025-01-02 18:33:13 +0200

    Optimization

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,20 +1,22 @@
 // Refactor following solution
 // Filter array by type name
 
-const filter = (array, type) => {
-  let remove = [];
+// const filter = (array, type) => {
+//   let remove = [];
 
-  for (item of array) {
-    const itemIndex = array.indexOf(item);
-    if (typeof array[itemIndex] !== type) {
-      remove.unshift(itemIndex);
-    }
-  }
+//   for (item of array) {
+//     const itemIndex = array.indexOf(item);
+//     if (typeof array[itemIndex] !== type) {
+//       remove.unshift(itemIndex);
+//     }
+//   }
 
-  for (item of remove) array.splice(item, 1);
+//   for (item of remove) array.splice(item, 1);
 
-  return array;
-};
+//   return array;
+// };
+
+
+const filter = (array, type) => array.filter(item => typeof item === type)
 
 module.exports = filter;
-git 
