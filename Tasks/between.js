7c422ea6b7c7c344f8cd67c77873a0a3dff7ed59
pc <lovekuchernazarlove@gmail.com>
2025-01-02 12:04:22 +0200

    Improve Naming Conventions:
    - Use camelCase for function and variable names.
    - Make variable names more descriptive.

--- Tasks/between.js
+++ Tasks/between.js
@@ -2,23 +2,23 @@
 // Extract substring between prefix and suffix
 'use strict';
 
-const getValueBetween = (str, p, s) => {
-  const i = str.indexOf(p);
-  if (i === -1) return '';
+const getValueBetween = (inputString, prefix, suffix) => {
+  const prefixIndex = inputString.indexOf(prefix);
+  if (prefixIndex === -1) return '';
   else {
-    const k = i + p.length;
-    str = str.substring(k);
-    if (s) {
-      const innerIndex = str.indexOf(s);
-      if (innerIndex === -1) {
+    const start = prefixIndex + prefix.length;
+    let resultString = inputString.substring(start);
+    if (suffix) {
+      const suffixIndex = resultString.indexOf(suffix);
+      if (suffixIndex === -1) {
         return '';
       } else {
-        str = str.substring(0, innerIndex);
+        resultString = resultString.substring(0, suffixIndex);
       }
     }
+    return resultString;
   }
-  return str;
 };
 
 
-module.exports = getvaluebetween;
+module.exports = getValueBetween;
