a86f577b9b3954e673e4c14ce462cad52c5e2bc4
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-02 02:29:00 +0200

    2. improve naming

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,13 +1,13 @@
 // Refactor following solution
 // Get one random element from an array
 
-// Step 1. add "use strict"
+// Step 2. improve naming
 
 "use strict";
 
-const SAMPLE = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
+const sample = (array) => {
+  array = array[Math.floor(Math.random() * array.length)];
+  return array;
 };
 
-module.exports = SAMPLE;
+module.exports = sample;
