2551cd2bc2b1e642b4336ba9b56cc5ace7be57f5
pc <lovekuchernazarlove@gmail.com>
2025-01-02 13:15:41 +0200

    Introduce NOT_FOUND constant in parseDay function
    - Replaced magic number `-1` with `NOT_FOUND`

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,12 +3,13 @@
 'use strict';
 
 const dayAbbreviations = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const NOT_FOUND = -1;
 
 /**
  * Parses the input string to determine the corresponding day number.
  *
  * @param {string} inputString - The string to parse for the day abbreviation.
- * @returns {number} - Returns the day number (1 for Sun, 2 for Mon, ..., 7 for Sat). Returns -1 if no match is found.
+ * @returns {number} - Returns the day number (1 for Sun, 2 for Mon, ..., 7 for Sat). Returns `NOT_FOUND` if no match is found.
  * @throws {TypeError} - Throws if `inputString` is not a non-empty string.
  */
 const parseDay = (inputString) => {
@@ -17,7 +18,7 @@ const parseDay = (inputString) => {
   }
 
   const index = dayAbbreviations.findIndex(day => inputString.startsWith(day.toLowerCase()));
-  return index !== -1 ? index + 1 : -1;
+  return index !== NOT_FOUND ? index + 1 : NOT_FOUND;
 };
 
 module.exports = parseDay;
