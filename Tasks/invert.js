9d9b5e182663c823a113a2e8b734f0a242ff4bb1
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-02 14:39:18 +0200

    4. replace forEach-loop with for...

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,15 +1,14 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-// Step 3. fix naming and remove unused params, parts of code, "magic numbers"
+// Step 4. replace forEach-loop with for...
 
 "use strict";
 
 const invert = (array) => {
-  const result = Object.keys(array);
-  result.forEach((_, i) => {
-    result[i] = array.pop();
-  });
+  const result = [];
+  const length = array.length;
+  for (let i = 0; i < length; i++) result[i] = array.pop();
   return result;
 };
 
