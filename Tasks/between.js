ed70687192357f71cf249649935029d88a12c516
pc <lovekuchernazarlove@gmail.com>
2025-01-02 12:05:49 +0200

    Use intermediate variables instead of reassigning str.

--- Tasks/between.js
+++ Tasks/between.js
@@ -5,20 +5,21 @@
 const getValueBetween = (inputString, prefix, suffix) => {
   const prefixIndex = inputString.indexOf(prefix);
   if (prefixIndex === -1) return '';
-  else {
-    const start = prefixIndex + prefix.length;
-    let resultString = inputString.substring(start);
-    if (suffix) {
-      const suffixIndex = resultString.indexOf(suffix);
-      if (suffixIndex === -1) {
-        return '';
-      } else {
-        resultString = resultString.substring(0, suffixIndex);
-      }
+
+  const start = prefixIndex + prefix.length;
+  const substringAfterPrefix = inputString.substring(start);
+
+  if (suffix) {
+    const suffixIndex = substringAfterPrefix.indexOf(suffix);
+    if (suffixIndex === -1) {
+      return '';
     }
-    return resultString;
+    return substringAfterPrefix.substring(0, suffixIndex);
   }
+
+  return substringAfterPrefix;
 };
+
 
 
 module.exports = getValueBetween;
