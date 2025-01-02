ce67db87ae74b5dd3d2cfc726106b2e7e0f36087
Liza-Khube <elisekhu@gmail.com>
2025-01-02 19:30:04 +0200

    Implement the function with recursion

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
 
 // Implement following task
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
 
-const schedule = async (func, n, interval) => {};
+const schedule = async (func, n, interval, i = 0) => {
+  if (i >= n) return;
+  func(i);
+  await new Promise((res) => setTimeout(res, interval));
+  await schedule(func, n, interval, ++i);
+};
 
 module.exports = schedule;
