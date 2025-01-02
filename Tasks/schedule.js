bb03ba6901364c56236ee8e63191f26f8c87cc6c
Maksim Ostroginskiy <ostroginskiymaksim@gmail.com>
2025-01-02 20:45:42 +0200

    Implementation

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
 
 // Implement following task
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
 
-const schedule = async (func, n, interval) => {};
-
-module.exports = schedule;
+const schedule = async (func, n, interval) => {
+  for (let i = 0; i < n; i++) {
+    func(i);
+    await new Promise((resolve) => setTimeout(resolve, interval));
+  }
+};
+  
+  module.exports = schedule;
