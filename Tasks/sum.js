ac4e1b497c878e416ec9e97a32b05c6803e2659e
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-02 02:49:59 +0200

    1. add "use strict"

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,12 +1,16 @@
 // Refactor following solution
 // Sum all numbers from an array
 
+// Step 1. add "use strict"
+
+"use strict";
+
 const k = (sum = function (s = () => {}) {
   sum = [0];
   let k = 5;
   for (i of s) {
     let t = typeof i;
-    if (t === 'number') {
+    if (t === "number") {
       if (sum.length > 0) {
         const new_Sum = sum[sum.length - 1] + i;
         sum.push(new_Sum);
