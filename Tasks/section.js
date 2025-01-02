c776b5e153aea2211cd085522f4ee61d36332ee9
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-02 14:17:48 +0200

    add "use strict"

--- Tasks/section.js
+++ Tasks/section.js
@@ -1,10 +1,14 @@
 // Refactor following solution
 // Split string into array by the first occurrence of separator
 
+// Step 1. add "use strict"
+
+"use strict";
+
 SectionString = (s, t) => {
   i = s.indexOf(t);
-  if (i < 0 || t == '') {
-    return [s, ''];
+  if (i < 0 || t == "") {
+    return [s, ""];
   } else {
     return [s.slice(0, i), s.slice(i + t.length)];
   }
