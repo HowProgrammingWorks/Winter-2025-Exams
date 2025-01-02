202c0278fb5947ddf92fc4dce2bb64549d461152
Oleksandr Cherepov <sashacherepov55@gmail.com>
2025-01-02 11:56:01 +0200

    Refactor: fix name collision

--- Tasks/only.js
+++ Tasks/only.js
@@ -2,8 +2,8 @@
 
 'use strict';
 
-const only = (dict, ...only) => {
-  const keys = Object.keys(dict).filter((key) => only.includes(key));
+const only = (dict, ...toCopy) => {
+  const keys = Object.keys(dict).filter((key) => toCopy.includes(key));
   return keys.reduce((acc, key) => ({ ...acc, [key]: dict[key] }), {});
 };
 
