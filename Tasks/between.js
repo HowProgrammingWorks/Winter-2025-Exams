d76583f95cb2a1553708772b34938345091c81a8
Maksim Ostroginskiy <ostroginskiymaksim@gmail.com>
2025-01-02 15:00:12 +0200

    -

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,7 +1,7 @@
 // Refactor following solution
 // Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
+test = (str, p, s) => {
   i = str.indexOf(p);
   if (i === -1) return '';
   else {
