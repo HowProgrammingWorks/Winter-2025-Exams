ca4a3c821b9532701d9331d120b2311d6b686f53
pc <lovekuchernazarlove@gmail.com>
2025-01-02 13:03:20 +0200

    Add Input Validation and JSDocs

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,7 +2,18 @@
 // Sum all number values in dict
 'use strict';
 
+/**
+ * Sums all numeric values in an object.
+ *
+ * @param {Object} sourceObj - The object to sum numeric values from.
+ * @returns {number} - The total sum of numeric values.
+ * @throws {TypeError} - Throws if `sourceObj` is not a non-null object.
+ */
 const sumNumericValues = (sourceObj) => {
+  if (typeof sourceObj !== 'object' || sourceObj === null) {
+    throw new TypeError('Argument must be a non-null object');
+  }
+
   const objectKeys = Object.keys(sourceObj);
   const total = objectKeys.reduce((accumulator, key) => {
     const value = sourceObj[key];
