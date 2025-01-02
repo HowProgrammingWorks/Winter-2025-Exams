ccec698d0f7ec24935604b5e73e4920ea6b2c755
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-02 14:32:38 +0200

    4. rewrite into functional style with ternary operator

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,19 +1,17 @@
 // Refactor following solution
 // Get day number
 
-// Step 3. fix naming
+// Step 4. refix naming and rewrite into functional style with ternary operator
 
 "use strict";
 
 const DAYS_OF_WEEK = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const getDayNumber = (day) => {
-  for (let index = 0; index < DAYS_OF_WEEK.length; index++) {
-    if (day.startsWith(DAYS_OF_WEEK[index].toLowerCase())) {
-      return index + 1;
-    }
-  }
-  return -1;
+  const index = DAYS_OF_WEEK.findIndex((substr) =>
+    day.startsWith(substr.toLowerCase()),
+  );
+  return index !== -1 ? index + 1 : -1;
 };
 
 module.exports = getDayNumber;
