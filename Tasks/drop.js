c3492732c8968af635a95127ee6d9daebee93a88
pc <lovekuchernazarlove@gmail.com>
2025-01-02 13:49:54 +0200

    Add Input Validation and JSDocs

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -2,7 +2,25 @@
 // Delete listed keys from dictionary
 'use strict';
 
+/**
+ * Removes specified keys from a dictionary.
+ *
+ * @param {Object} dictionary - The object from which keys will be removed.
+ * @param {...string} keysToDelete - The keys to remove from the dictionary.
+ * @returns {Object} - The modified dictionary with specified keys removed.
+ * @throws {TypeError} - Throws if `dictionary` is not a non-null object or if any key to delete is not a string.
+ */
 const removeKeys = (dictionary, ...keysToDelete) => {
+  if (typeof dictionary !== 'object' || dictionary === null) {
+    throw new TypeError('First argument must be a non-null object');
+  }
+
+  for (const key of keysToDelete) {
+    if (typeof key !== 'string') {
+      throw new TypeError('All keys to delete must be strings');
+    }
+  }
+
   const keysToDeleteSet = new Set(keysToDelete);
   Object.keys(dictionary).forEach((key) => {
     if (keysToDeleteSet.has(key)) {
@@ -11,6 +29,5 @@ const removeKeys = (dictionary, ...keysToDelete) => {
   });
   return dictionary;
 };
-
 
 module.exports = removeKeys;
