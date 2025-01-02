e468c573f6304fa4608eb4d9ed906a1c4ad8b9c3
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-02 02:26:59 +0200

    1. add "use strict"

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,6 +1,10 @@
 // Refactor following solution
 // Get one random element from an array
 
+// Step 1. add "use strict"
+
+"use strict";
+
 const SAMPLE = (arr) => {
   arr = arr[Math.floor(Math.random() * arr.length)];
   return arr;
