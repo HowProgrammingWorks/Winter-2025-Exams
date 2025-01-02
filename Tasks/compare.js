4fc5dacedacb34b43b28a9c26a5817ddfab02e0c
pc <lovekuchernazarlove@gmail.com>
2025-01-02 12:57:39 +0200

    IDK, Can we change contract of our function? if we can, here one more version

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -10,16 +10,50 @@
  * @returns {boolean} - Returns `true` if both dictionaries have the same keys in the same order and corresponding values, otherwise `false`.
  * @throws {TypeError} - Throws if either `firstDict` or the first object in `parametersList` is not a non-null object.
  */
-const compareDictionaries = (firstDict, ...parametersList) => {
+// const compareDictionaries = (firstDict, ...parametersList) => {
+//   if (
+//     typeof firstDict !== 'object' || firstDict === null ||
+//     parametersList.length === 0 ||
+//     typeof parametersList[0] !== 'object' || parametersList[0] === null
+//   ) {
+//     throw new TypeError('Both arguments must be non-null objects');
+//   }
+//
+//   const secondDict = parametersList[0];
+//   const firstKeys = Object.keys(firstDict);
+//   const secondKeys = Object.keys(secondDict);
+//
+//   if (firstKeys.length !== secondKeys.length) return false;
+//
+//   for (let i = 0; i < firstKeys.length; i++) {
+//     const key1 = firstKeys[i];
+//     const key2 = secondKeys[i];
+//
+//     if (key1 !== key2 || firstDict[key1] !== secondDict[key2]) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// };
+
+
+/**
+ * Compares two dictionaries (objects) for equality, including key order.
+ *
+ * @param {Object} firstDict - The first dictionary to compare.
+ * @param {Object} secondDict - The second dictionary to compare.
+ * @returns {boolean} - Returns `true` if both dictionaries have the same keys in the same order and corresponding values, otherwise `false`.
+ * @throws {TypeError} - Throws if either `firstDict` or `secondDict` is not a non-null object.
+ */
+const compareDictionaries = (firstDict, secondDict) => {
   if (
     typeof firstDict !== 'object' || firstDict === null ||
-    parametersList.length === 0 ||
-    typeof parametersList[0] !== 'object' || parametersList[0] === null
+    typeof secondDict !== 'object' || secondDict === null
   ) {
     throw new TypeError('Both arguments must be non-null objects');
   }
 
-  const secondDict = parametersList[0];
   const firstKeys = Object.keys(firstDict);
   const secondKeys = Object.keys(secondDict);
 
@@ -36,5 +70,6 @@ const compareDictionaries = (firstDict, ...parametersList) => {
 
   return true;
 };
+
 
 module.exports = compareDictionaries;
