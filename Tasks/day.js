949f6520a62cc79b370f5baee49b222736eaa555
Anastasia Zhuravel <sherloch3k@gmail.com>
2025-01-03 13:23:46 +0200

    empty blocks

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,8 +1,8 @@
 // Refactor following solution
 // Get day number
 
-// Step 2
-// add const and let
+// Step 3
+// remove empty and useless blocks
 
 'use strict';
 
@@ -10,9 +10,7 @@ const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 parseDay = (day) => {
   for (let i = 0; i < days.length; i++) {
-    if (day.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
+    if (day.startsWith(days[i].toLowerCase)) return i + 1;
   }
   return -1;
 };
