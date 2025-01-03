379a6383e4d643a30a5e17cd828532ec8d611ab0
Anastasia Zhuravel <sherloch3k@gmail.com>
2025-01-03 13:13:26 +0200

    const and let

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,17 +1,15 @@
 // Refactor following solution
 // Get day number
 
-// Step 1
-// add use strict
-// rename variables
+// Step 2
+// add const and let
 
 'use strict';
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 parseDay = (day) => {
-  let i;
-  for (i = 0; i < days.length; i++) {
+  for (let i = 0; i < days.length; i++) {
     if (day.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
