76140aec4e7c8c6e17c1d0f6b472dcea0d1942a7
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2025-01-03 23:19:02 +0200

    Add eslint-config-metarhia

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const eslintConfig = require("eslint-config-metarhia");
+
+/** @type {import('eslint').Linter.Config[]} */
+module.exports = [
+  ...eslintConfig,
+  {
+    rules: {
+      "no-undef": "warn"
+    }
+  }
+];
