3f5a8f4da93b390dd67951e54b94e5e4ae889ceb
Sonya010 <kozachsofia@gmail.com>
2025-01-03 18:45:39 +0200

    commit

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,13 +1,12 @@
 // Refactor following solution
 // Generate random integer value in given range
 
-// Step 1
-// Add 'use strict'
-// Add `const`
+// Step 2
+// Assign values to max
 
 'use strict';
 
-const Random = (min, max) => {
+const Random = (min, max = min) => {
   if (max === undefined) {
     max = min;
     return Math.floor(Math.random() * (max + 1));
