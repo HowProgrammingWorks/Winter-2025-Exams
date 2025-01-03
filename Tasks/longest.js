ba1c7a843a615358e3221d1bd4037ccf0b16b5ac
Liza-Khube <elisekhu@gmail.com>
2025-01-03 22:43:58 +0200

    Add check if given array is empty

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -4,6 +4,7 @@
 "use strict";
 
 const longest = (arrayOfStr) => {
+  if (arrayOfStr.length === 0) return null;
   return arrayOfStr.reduce((prevStr, currStr) =>
     prevStr.length < currStr.length ? currStr : prevStr
   );
