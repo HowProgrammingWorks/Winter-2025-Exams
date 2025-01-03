d5331850fdf58b9bfb62d09faecdd130e6f00441
Liza-Khube <elisekhu@gmail.com>
2025-01-03 22:41:18 +0200

    Replace for..of and if-else statements with .reduce

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -4,13 +4,9 @@
 "use strict";
 
 const longest = (arrayOfStr) => {
-  let longestLength = -1;
-  let longestStr;
-  for (let str of arrayOfStr) {
-    (str.length > longestLength) ? longestLength = str.length : str = 0;
-    (str.length >= longestLength) ? longestStr = str : str = -1;
-  };
-  return longestStr;
+  return arrayOfStr.reduce((prevStr, currStr) =>
+    prevStr.length < currStr.length ? currStr : prevStr
+  );
 };
 
 module.exports = longest;
