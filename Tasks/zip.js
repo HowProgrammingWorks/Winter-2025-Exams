84622822e496462e297dd39dfeae2b241d632074
Anastasia Zhuravel <sherloch3k@gmail.com>
2025-01-03 20:10:27 +0200

    change function logic

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,25 +1,15 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 1
-// add use strict
-// rewrite function to arrow
+// Step 2
+// add variable minLength for future constraint of loop
+// add variable zipped for handling work of zipping logic
 
 'use strict';
 
 const zip = (a = [], b = []) => {
-  let i = 0;
-  j = 0;
-  for (x of b) {
-    CELL = [a[i++], x];
-    if (i < j) {
-      delete a[i++];
-    } else {
-      (() => (b[j++] = CELL))();
-    }
-    if (CELL[0] == undefined) b.length -= 1;
-  }
-  return b;
+  const minLength = Math.min(a.length, b.length);
+  const zipped = [];
 };
 
 module.exports = zip;
