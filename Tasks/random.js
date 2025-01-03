09101cfcf294a281da085e403fa3b4e4b56ff283
Sonya010 <kozachsofia@gmail.com>
2025-01-03 18:58:05 +0200

    commit

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,16 +1,14 @@
 // Refactor following solution
 // Generate random integer value in given range
 
-// Step 2
-// Assign values to max
+// Step 3
+// Change if logic
 
 'use strict';
 
 const Random = (min, max = min) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
+  if (min === max) return min;
+  else {
     return min + Math.floor(Math.random() * (max - min + 1));
   }
 };
