0af6eb0ac7c6b5e78a9902cd6d1a329c1d5985ae
Anastasia Zhuravel <sherloch3k@gmail.com>
2025-01-03 20:12:10 +0200

    add loop

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,15 +1,20 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 2
-// add variable minLength for future constraint of loop
-// add variable zipped for handling work of zipping logic
+// Step 3
+// add loop for zipping
 
 'use strict';
 
 const zip = (a = [], b = []) => {
   const minLength = Math.min(a.length, b.length);
   const zipped = [];
+
+  for (let i = 0; i < minLength; i++) {
+    zipped.push([a[i], b[i]]);
+  }
+
+  return zipped;
 };
 
 module.exports = zip;
