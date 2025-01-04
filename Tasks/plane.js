058beedb76a50b44f626d58285ceaabda86a83cd
Liza-Khube <elisekhu@gmail.com>
2025-01-04 18:16:34 +0200

    Fix formatting

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -3,19 +3,20 @@
 
 "use strict";
 
-const_plane = function(arr, res=[]) {
-  j = 0; for(let i = 0, length = arr.length; i < length; i++){
+const_plane = function (arr, res = []) {
+  j = 0;
+  for (let i = 0, length = arr.length; i < length; i++) {
     value = arr[i];
-    j = i
+    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1];
+    } else {
+      arr[i] = res[j - 1];
+      res.push(value);
     };
   };
-  return res
-}
+  return res;
+};
 
 module.exports = const_plane;
