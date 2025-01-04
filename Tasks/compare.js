0139384f31f3d6e10fb51473646c4d73743c20c4
Ярослав <yatchenkoyaroslav2019@gmail.com>
2025-01-04 07:23:33 +0200

    change the method of objects

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,12 +1,12 @@
-// step3
-//deleted useless code
+// step4
+//change the method of objects
 
 'use strict';
 
 let compare = (first_values, ...parameters_LIST) => {
-  const firstKeys = Object.keys(first_values);
-  const secondKeys = Object.keys(parameters_LIST[0]);
+  const firstKeys = Object.entries(first_values);
+  const secondKeys = Object.entries(parameters_LIST[0]);
 
-  return (firstKeys.join('-') === secondKeys.join('-'))
+  return firstKeys.join('-') === secondKeys.join('-');
 };
 module.exports = compare;
