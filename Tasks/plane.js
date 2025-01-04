151fbfe7c02a2334d19a6f08ec91eaca57743761
Liza-Khube <elisekhu@gmail.com>
2025-01-04 18:13:27 +0200

    Add 'use strict'

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,6 +1,8 @@
 // Refactor following solution
 // Make nested array plane
 
+"use strict";
+
 const_plane = function(arr, res=[]) {
   j = 0; for(let i = 0, length = arr.length; i < length; i++){
     value = arr[i];
