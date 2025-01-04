0a0e74944f88cd7fc6d35b80a0a0e03d5f5b6070
Ярослав <yatchenkoyaroslav2019@gmail.com>
2025-01-04 04:12:36 +0200

    edit the if condition

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,14 +1,14 @@
 // Step 3
-//entering the for..of loop
+//edit the if condition
 
 'use strict';
 
-let DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (s) => {
-  for (const day of DAYS ) {
-    if (s.startsWith(DAYS[day].toLowerCase())) {
-      return day + 1;
+  for (const day of DAYS) {
+    if (s.startsWith(day.toLowerCase())) {
+      return DAYS.indexOf(day) + 1;
     }
   }
   return -1;
