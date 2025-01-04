bf1d81af932437151a10303f5dc4a09449722667
Liza-Khube <elisekhu@gmail.com>
2025-01-04 19:15:48 +0200

    Remove unneeded variable 'j' and assignments

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -4,15 +4,11 @@
 "use strict";
 
 const const_plane = (arr, res = []) => {
-  let j = 0;
   for (let i = 0, length = arr.length; i < length; i++) {
     const value = arr[i];
-    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
       res.push(...const_plane(value));
-      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1];
       res.push(value);
     };
   };
