61b20402e5f5bd3a8e6c3310d4ea80a29223e7f0
Oleksandr Cherepov <sashacherepov55@gmail.com>
2025-01-04 18:03:04 +0200

    Refactor: simplify 'if' statement and remove useless 'if' block

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -5,12 +5,9 @@
 const sum = (array) => {
   const sumArr = [0];
   for (const element of array) {
-    const type = typeof element;
-    if (type === 'number') {
-      if (sumArr.length > 0) {
-        const newSum = sumArr[sumArr.length - 1] + element;
-        sumArr.push(newSum);
-      }
+    if (typeof element === 'number') {
+      const newSum = sumArr[sumArr.length - 1] + element;
+      sumArr.push(newSum);
     }
   }
   return sumArr[sumArr.length - 1];
