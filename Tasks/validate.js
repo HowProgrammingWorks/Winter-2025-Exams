d566e75342314d61f1dc2646d12e3b65852b6e20
Oleksandr Cherepov <sashacherepov55@gmail.com>
2025-01-04 18:40:31 +0200

    Refactor: rename parameters and remove useless blocks

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -2,25 +2,19 @@
 
 'use strict';
 
-const isValidate = (T) => {
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
-  {
-    for (const C of T) {
-      if (C === ' ') continue;
-      if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
-      ) {
-      } else {
-        return false;
-      }
+const isValidate = (name) => {
+  if (!name.includes(' ')) return false;
+  for (const char of name) {
+    if (char === ' ') continue;
+    if (
+      char.toLowerCase().charCodeAt(0) >= 97 &&
+      char.toLowerCase().charCodeAt(0) <= 122
+    ) {
+    } else {
+      return false;
     }
-    return true;
   }
+  return true;
 };
 
 module.exports = isValidate;
