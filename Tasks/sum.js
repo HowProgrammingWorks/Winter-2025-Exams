7b3926af03b0c56b5b32f109091b6bdc405de622
Oleksandr Cherepov <sashacherepov55@gmail.com>
2025-01-04 18:00:11 +0200

    Refactor: change identifiers' case

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,18 +2,18 @@
 
 'use strict';
 
-const sum = (s) => {
-  const sum = [0];
-  for (const i of s) {
-    const t = typeof i;
-    if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
+const sum = (array) => {
+  const sumArr = [0];
+  for (const element of array) {
+    const type = typeof element;
+    if (type === 'number') {
+      if (sumArr.length > 0) {
+        const newSum = sumArr[sumArr.length - 1] + element;
+        sumArr.push(newSum);
       }
     }
   }
-  return sum[sum.length - 1];
+  return sumArr[sumArr.length - 1];
 };
 
 module.exports = sum;
