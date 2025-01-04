564cf1ffaf34f6198a4fc8d4f769b1ac8ef71559
Ярослав <yatchenkoyaroslav2019@gmail.com>
2025-01-04 05:23:44 +0200

    formatting

--- Tasks/range.js
+++ Tasks/range.js
@@ -1,13 +1,13 @@
-// step3
-//add Conditional operator
+// step4
+//formatting
 
 'use strict';
 
 const range = (...Range) => {
   const [from, to] = Range;
   return to < from
-      ? []
-      : Array.from({length: to - from + 1}, (_, i) => from+i);
+    ? []
+    : Array.from({ length: to - from + 1 }, (_, i) => from + i);
 };
 
 module.exports = range;
