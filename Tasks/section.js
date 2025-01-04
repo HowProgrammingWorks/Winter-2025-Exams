e4e7a6b7db8c8f02cc5a6edd2969925377c9be67
Ярослав <yatchenkoyaroslav2019@gmail.com>
2025-01-04 03:38:41 +0200

    return new TypeError

--- Tasks/section.js
+++ Tasks/section.js
@@ -1,14 +1,17 @@
-// Step 3
-//entering a conditional operator
+// Step 4
+//return new TypeError
 
 'use strict';
 
 const sectionString = (s, t) => {
-  const sectionPosition = s.indexOf(t);
+  if (typeof s !== 'string' || typeof t !== 'string') {
+    throw new TypeError('Both arguments must be strings');
+  }
 
-  return sectionPosition < 0 || t ===''
-      ?[s, '']
-      :[s.slice(0, sectionPosition), s.slice(sectionPosition + t.length)];
+  const sectionPosition = s.indexOf(t);
+  return sectionPosition < 0 || t === ''
+    ? [s, '']
+    : [s.slice(0, sectionPosition), s.slice(sectionPosition + t.length)];
 };
 
 module.exports = sectionString;
