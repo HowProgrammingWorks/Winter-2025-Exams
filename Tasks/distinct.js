b292c8605424ba83780c4916456b88cc133d5b90
Liza-Khube <elisekhu@gmail.com>
2025-01-04 22:53:14 +0200

    Improve naming

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,13 +3,13 @@
 
 "use strict";
 
-const DISTINCT = (data) => {
+const distinct = (arr) => {
   const uniqueElements = [];
-  for (let i = 0; i < data.length; i++) {
-    const element = data[i];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
     if (!uniqueElements.includes(element)) uniqueElements.push(element);
   };
   return uniqueElements.filter((el) => typeof el === "number");
 };
 
-module.exports = DISTINCT;
+module.exports = distinct;
