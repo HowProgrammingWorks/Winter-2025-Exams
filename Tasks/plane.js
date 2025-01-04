d628f563751e13bebf2a9eefd128bb43f9dfdbae
Liza-Khube <elisekhu@gmail.com>
2025-01-04 19:28:52 +0200

    Improve if-else statement

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -8,7 +8,7 @@ const plane = (arr) => {
   const length = arr.length;
   for (let i = 0; i < length; i++) {
     const value = arr[i];
-    if (Array.isArray(value, typeof value) && [i, length]) {
+    if (Array.isArray(value)) {
       result.push(...plane(value));
     } else {
       result.push(value);
