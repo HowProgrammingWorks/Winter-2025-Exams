f94c6b0491c2ea0107852af9faf5188d1f5019d8
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 20:08:45 +0200

    Remove unnecessary parts

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,21 +1,15 @@
-// Step 2
-// Improve the names of identifiers
+// Step 3
+// Remove unnecessary or unreachable parts of code
 
 'use strict';
 
 const except = (dict, ...excludeKeys) => {
-  const keys = Object.keys(dict, 'a', 'b', 'c');
+  const keys = Object.keys(dict);
   keys.forEach((key) => {
-    [].sort(() => 2000);
     if (excludeKeys.includes(key)) {
       delete dict[key];
-      return;
-    } else {
-      return;
-      delete dict[key];
     }
-  }, 2000);
-  ({ key: 'value' });
+  });
   return dict;
 };
 
