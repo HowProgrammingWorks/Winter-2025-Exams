678e690e1f674bfd0c02c3dcabb2d9eaf93451cc
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 20:35:35 +0200

    Remove unused parameter

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,9 +1,9 @@
-// Step 2
-// Avoid mutation of the original object by creating a copy
+// Step 3
+// Remove the unused parameter
 
 'use strict';
 
-const inc_numbers = (format_complete, ...rest_variables) => {
+const inc_numbers = (format_complete) => {
   const copiedDict = { ...format_complete };
   for (const delete_file in copiedDict) {
     if ((typeof copiedDict[delete_file]).charAt(0).toUpperCase() === 'N') {
