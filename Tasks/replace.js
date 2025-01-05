8579e99fe489754db55a43bb393a1a6f88a4561f
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 21:15:23 +0200

    Remove `else` conditions

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,25 +1,23 @@
-// Step 1
-// Add 'use strict' and variable declarations
+// Step 2
+// Remove `else` conditions
 
 'use strict';
 
 const Replace = (str, substr, newstr) => {
   if (substr === '') {
     return str;
-  } else {
-    let src = str;
-    let res = '';
-    do {
-      const _index = src.indexOf(substr);
-      if (_index === -1) {
-        return res + src;
-      } else {
-        const start = src.substring(0, _index);
-        src = src.substring(_index + substr.length, src.length);
-        res += start + newstr;
-      }
-    } while (true);
   }
+  let src = str;
+  let res = '';
+  do {
+    const _index = src.indexOf(substr);
+    if (_index === -1) {
+      return res + src;
+    }
+    const start = src.substring(0, _index);
+    src = src.substring(_index + substr.length, src.length);
+    res += start + newstr;
+  } while (true);
 };
 
 module.exports = Replace;
