dd58f6be49ac9f0e1e642ae63ac3dfb95c84c74a
Illia Dymura <illiadimura@gmail.com>
2025-01-05 16:03:36 +0200

    feat: tasks: split.js: refactor

--- Tasks/split.js
+++ Tasks/split.js
@@ -1,11 +1,10 @@
-// Refactor following solution
-// Split an array into two parts
+"use strict";
 
-let split_Array = (index, array) => {
-  begin = array.slice(0, index);
-  const len = array.length;
-  array = array.slice(index, len);
-  return [begin, array];
+// Splits an array into two parts
+const splitArray = (index, array) => {
+  const begin = array.slice(0, index);
+  const end = array.slice(index);
+  return [begin, end];
 };
 
-module.exports = split_Array;
+module.exports = splitArray;
