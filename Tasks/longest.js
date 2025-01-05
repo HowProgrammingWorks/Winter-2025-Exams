52404501203fc798754106349b0c822b0f0a5faf
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 21:02:16 +0200

    Handle empty arrays

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -1,9 +1,10 @@
-// Step 5
-// Improve iteration
+// Step 6
+// Handle empty arrays
 
 'use strict';
 
 const longest = (line = []) => {
+  if (!line.length) return null;
   let s = line[0];
   for (const i of line) {
     if (i.length > s.length) {
