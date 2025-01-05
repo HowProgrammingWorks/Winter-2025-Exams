abf85f2a9704b66a2aaebb8c06919922d27dde47
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 22:09:46 +0200

    Remove unnecessary parts

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,12 +1,10 @@
-// Step 1
-// Add 'use strict' and variable declarations
-// Delete the variable `k`, which is used for the function
+// Step 2
+// Delete unused and unnecessary parts
 
 'use strict';
 
-const sum = function (s = () => {}) {
+const sum = function (s) {
   let sum = [0];
-  let k = 5;
   for (const i of s) {
     let t = typeof i;
     if (t === 'number') {
@@ -16,9 +14,7 @@ const sum = function (s = () => {}) {
       }
     }
   }
-  sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
 };
 
 module.exports = sum;
