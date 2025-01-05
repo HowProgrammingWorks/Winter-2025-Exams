129a0cad4e58954783681aaafa4a201de8ce0615
Illia Dymura <illiadimura@gmail.com>
2025-01-05 16:02:42 +0200

    feat: tasks: sum.js: refactor

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,21 +1,16 @@
-// Refactor following solution
-// Sum all numbers from an array
+"use strict";
 
-const k = (sum = function (s = () => {}) {
-  sum = [0];
-  let k = 5;
-  for (i of s) {
-    let t = typeof i;
-    if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
-      }
+// Sums up all numbers from an array
+const sum = (arr) => {
+  let sum = 0;
+
+  for (const item of arr) {
+    if (typeof item === "number") {
+      sum += item;
     }
   }
-  sum[0];
-  return sum[sum.length - 1];
-  sum.push(sum.length - 1);
-});
+
+  return sum;
+};
 
 module.exports = sum;
