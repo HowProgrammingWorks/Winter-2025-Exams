7b3c27b27434cc5f33c1e98498fdcec32b3a44e6
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 21:16:41 +0200

    Remove infinity loop and simplify the code

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,5 +1,6 @@
-// Step 2
-// Remove `else` conditions
+// Step 3
+// Remove infinity loop
+// Simplify the code
 
 'use strict';
 
@@ -8,16 +9,14 @@ const Replace = (str, substr, newstr) => {
     return str;
   }
   let src = str;
-  let res = '';
-  do {
-    const _index = src.indexOf(substr);
-    if (_index === -1) {
-      return res + src;
-    }
-    const start = src.substring(0, _index);
-    src = src.substring(_index + substr.length, src.length);
-    res += start + newstr;
-  } while (true);
+  const _index = src.indexOf(substr);
+  if (_index === -1) {
+    return src;
+  }
+  const start = src.substring(0, _index);
+  src = src.substring(_index + substr.length, src.length);
+  const res = start + newstr + src;
+  return res;
 };
 
 module.exports = Replace;
