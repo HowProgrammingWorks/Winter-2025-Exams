d028b5442e6d78e807dfb386fe9219fe37741d4a
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 22:13:48 +0200

    Remove `length` condition and `type` declaration

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,17 +1,15 @@
-// Step 3
-// Improve the names of identifiers
+// Step 4
+// Delete the `length` condition
+// Delete the `type` declaration
 
 'use strict';
 
 const sumNums = function (array) {
   let sum = [0];
   for (const element of array) {
-    let type = typeof element;
-    if (type === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + element;
-        sum.push(new_Sum);
-      }
+    if (typeof element === 'number') {
+      const new_Sum = sum[sum.length - 1] + element;
+      sum.push(new_Sum);
     }
   }
   return sum[sum.length - 1];
