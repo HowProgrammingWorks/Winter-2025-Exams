e348182e17236983341432e10e6d56b9a4fdb19a
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 21:17:41 +0200

    Simplify and merge conditions

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,18 +1,12 @@
-// Step 3
-// Remove infinity loop
-// Simplify the code
+// Step 4
+// Simplify and merge conditions
 
 'use strict';
 
 const Replace = (str, substr, newstr) => {
-  if (substr === '') {
-    return str;
-  }
   let src = str;
   const _index = src.indexOf(substr);
-  if (_index === -1) {
-    return src;
-  }
+  if (!substr || _index === -1) return str;
   const start = src.substring(0, _index);
   src = src.substring(_index + substr.length, src.length);
   const res = start + newstr + src;
