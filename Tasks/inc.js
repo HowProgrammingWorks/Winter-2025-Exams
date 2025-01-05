7f21e2f5422e9c55543d215aec3e67de171bfb31
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 20:38:58 +0200

    Use increment

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,5 +1,5 @@
-// Step 5
-// Simplify the `typeof` condition by removing unnecessary string methods
+// Step 6
+// Use the increment operator instead of `=`
 
 'use strict';
 
@@ -7,7 +7,7 @@ const incNumbers = (dict) => {
   const copiedDict = { ...dict };
   for (const key in copiedDict) {
     if (typeof copiedDict[key] === 'number') {
-      copiedDict[key] = copiedDict[key] + 1;
+      copiedDict[key]++;
     }
   }
   return copiedDict;
