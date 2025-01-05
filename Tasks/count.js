a5d5b752a6707ed4f038f3bc4100e881c6e6e332
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 19:05:51 +0200

    Move function inside `reduce`

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,15 @@
-// Step 5
-// Improve iteration
-// Remove the `sum` variable
-// Use reduce method
+// Step 6
+// Decrease the size of the program
+// Move function initialization inside of the reduce method
 
 'use strict';
 
 const sumNums = (obj) => {
   const values = Object.values(obj);
-  const fn = (acc, value) => {
-    if (typeof value === 'number') acc += value;
-    return acc;
-  };
-  return values.reduce(fn, 0);
+  return values.reduce(
+    (acc, value) => (typeof value === 'number' ? acc + value : acc),
+    0,
+  );
 };
 
 module.exports = sumNums;
