e45fbd54e256ae9d90f8d20add9da14c0fa90e52
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 21:44:29 +0200

    Remove condition

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -1,14 +1,11 @@
-// Step 6
-// Improve methods
-// Remove String methods .indexOf() and .substring()
-// Use String.split() and Array methods .indexOf(), .join()
+// Step 7
+// Remove condition
 
 'use strict';
 
 const replaceWords = (str, substr, newStr) => {
   const words = str.split(' ');
   const index = words.indexOf(substr);
-  if (!substr || index === -1) return str;
   words[index] = newStr;
   return words.join(' ');
 };
