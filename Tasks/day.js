82cc091e2f53459283ae700ad9ad421cd9453fc1
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 19:53:28 +0200

    Improve iteration

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,14 +1,13 @@
-// Step 3
-// Change `WEEK` elements to LowerCase
-// Remove unnecessary .toLowerCase()
+// Step 4
+// Improve iteration
+// Move the `order` initialization inside of the loop
 
 'use strict';
 
 const WEEK = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const getNumOfDay = (input) => {
-  let order;
-  for (order = 0; order < WEEK.length; order++) {
+  for (let order = 0; order < WEEK.length; order++) {
     if (input.startsWith(WEEK[order])) {
       return order + 1;
     }
