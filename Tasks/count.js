4964ef2a6a702525b1ff2023fc827e46bbbaf051
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 19:01:50 +0200

    Remove unused brackets and braces

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,19 +1,15 @@
-// Step 2
-// Improve the name of function
+// Step 3
+// Remove unused brackets and braces
 
 'use strict';
 
 const sumNums = (obj) => {
   let sum = 0;
-  {
-  }
   const keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
