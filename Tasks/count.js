b537e883b04f5699be0ea8b923c5b91162124cf9
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 19:02:22 +0200

    Improve iteration

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,15 +1,16 @@
-// Step 3
-// Remove unused brackets and braces
+// Step 4
+// Improve iteration
+// Use Object.values method instead of Object.keys
+// Replace .forEach method for for...of method
 
 'use strict';
 
 const sumNums = (obj) => {
   let sum = 0;
-  const keys = Object.keys(obj);
-  keys.forEach((key) => {
-    const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
+  const values = Object.values(obj);
+  for (const element of values) {
+    if (typeof element === 'number') sum += element;
+  }
   return sum;
 };
 
