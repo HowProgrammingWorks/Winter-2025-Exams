6a7c44c7128a1c3005431e21b47a9ec93fb40ff9
Illia Dymura <illiadimura@gmail.com>
2025-01-05 15:58:56 +0200

    feat: tasks: validate.js: refactor

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,25 +1,6 @@
-// Refactor following solution
-// Validate person name
-
-const isValidate = (T) => {
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
-  {
-    for (C of T) {
-      if (C === ' ') continue;
-      if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
-      ) {
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
+// Validates person name
+const isValidPersonName = (name) => {
+  return /^[a-zA-Z]+( [a-zA-Z]+)+$/.test(name);
 };
 
-module.exports = isValidate;
+module.exports = isValidPersonName;
