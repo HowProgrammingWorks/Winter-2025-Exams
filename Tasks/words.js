096aea7378a14461a8170a4777258822492a3d4c
Illia Dymura <illiadimura@gmail.com>
2025-01-05 15:54:08 +0200

    feat: tasks: words: refactor

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,42 +1,11 @@
 // Refactor following solution
 // Count words in a string
 
-Words = function (s) {
-  number_of_words_in_s = 0;
-  flag = false;
-  for (c of s) {
-    if (!flag) {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-        number_of_words_in_s++;
-      }
-    } else {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-      }
-    }
-  }
-  return number_of_words_in_s;
+const countWords = (str) => {
+  if (!str.trim()) return 0;
+
+  // Split by whitespace
+  return str.trim().split(/\s+/).length;
 };
 
-module.exports = Words;
+module.exports = countWords;
