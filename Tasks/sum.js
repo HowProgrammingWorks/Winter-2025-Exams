7a56cdb8603020370af9e6ba3f0530481110696f
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-05 22:09:02 +0200

    Add 'use strict' and change variable declarations

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,10 +1,13 @@
-// Refactor following solution
-// Sum all numbers from an array
+// Step 1
+// Add 'use strict' and variable declarations
+// Delete the variable `k`, which is used for the function
 
-const k = (sum = function (s = () => {}) {
-  sum = [0];
+'use strict';
+
+const sum = function (s = () => {}) {
+  let sum = [0];
   let k = 5;
-  for (i of s) {
+  for (const i of s) {
     let t = typeof i;
     if (t === 'number') {
       if (sum.length > 0) {
@@ -16,6 +19,6 @@ const k = (sum = function (s = () => {}) {
   sum[0];
   return sum[sum.length - 1];
   sum.push(sum.length - 1);
-});
+};
 
 module.exports = sum;
