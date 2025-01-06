0f6b8d255fe4c431a048dc9c2508e958f7893397
Luba <luba.chernii@gmail.com>
2025-01-06 21:33:53 +0100

    imrpoved readability

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,12 @@
-// Refactor following solution
-// Get day number
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+const parseDay = (s) => {
+  for (let i = 0; i < DAYS.length; i++) {
+    if (s.startsWith(DAYS[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
