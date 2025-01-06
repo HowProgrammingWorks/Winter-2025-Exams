0898daca910ad3d89df6a767c70620eccf4ba44f
RickoBo <Vini.Marmone@gmail.com>
2025-01-07 01:51:01 +0200

    Step 5: prefer for..of instead forEach

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,15 +1,15 @@
 'use strict';
 // Reverse dict, exchange keys and values
-// Step 4
-// Improve naming
+// Step 5
+// Prefer for..of instead forEach
 
 const reverse = (obj) => {
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
     obj[value] = key;
     delete obj[key];
-  });
+  }
   return obj;
 };
 
