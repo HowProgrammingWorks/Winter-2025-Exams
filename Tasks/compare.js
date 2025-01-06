3729a311ca5f72ff416b20c24fb06789bd97c01a
inryceu <pavel.maluev@gmail.com>
2025-01-06 23:07:07 +0200

    added const, changing naming and incoming value

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -2,12 +2,10 @@
 // Compare two dictionaries
 "use strict";
 
-const compare = (first_values, ...parameters_LIST) => {
-  const second_values = parameters_LIST[0];
-  let a = Object.keys(first_values);
-  let b = Object.keys(second_values);
-  if (a.join('-') !== b.join('-')) return false;
-  let e = true;
+const compare = (first_values, second_values) => {
+  const a = Object.keys(first_values);
+  const b = Object.keys(second_values);
+  let result = true;
   for (c of a) {
     if (first_values[c] === second_values[c]) e = e && true;
     else {
