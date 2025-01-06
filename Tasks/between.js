caa87ab29f640376de9b6368d7bacde919c30f31
RickoBo <Vini.Marmone@gmail.com>
2025-01-07 00:59:49 +0200

    Step 5: remove else branching and 'selected' variable, simplify code

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,18 +1,15 @@
 'use strict';
-// Step 4
-// Declare variable 'selected'
-// Use 'selected' to not mutate incoming parameters
+// Step 5
+// Remove else branching and 'selected' variable
+// Simplify code
 
 const getValueBetween = (str, prefix, suffix) => {
-  let selected;
   const startIndex = str.indexOf(prefix) + 1;
   const endIndex = str.indexOf(suffix);
   if (startIndex === -1 || endIndex === -1) {
     return '';
-  } else {
-    selected = str.substring(startIndex, endIndex);
   }
-  return selected;
+  return str.substring(startIndex, endIndex);
 };
 
 module.exports = getValueBetween;
