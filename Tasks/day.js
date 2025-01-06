ee6fea798720e3d4453aa6eaa4e46d57ce0c6407
BalalaievMaxim <balalaev.maxim@lll.kpi.ua>
2025-01-06 23:41:43 +0200

    day.js: step 5

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,7 +1,7 @@
 // Refactor following solution
 // Get day number
 
-// Step 4: move counter definition into the loop
+// Step 5: used single-line if statement
 
 'use strict';
 
@@ -9,9 +9,7 @@ const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (str) => {
   for (let i = 0; i < DAYS.length; i++) {
-    if (str.startsWith(DAYS[i].toLowerCase())) {
-      return i + 1;
-    }
+    if (str.startsWith(DAYS[i].toLowerCase())) return i + 1;
   }
   return -1;
 };
