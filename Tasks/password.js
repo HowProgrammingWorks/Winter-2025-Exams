b15a6065d822fab859b3f17089825fe5ec64b679
BalalaievMaxim <balalaev.maxim@lll.kpi.ua>
2025-01-06 23:51:20 +0200

    password.js: step 5 (done)

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,7 +1,7 @@
 // Refactor following solution
 // Generate random password
 
-// Step 4: removed unnecessary intermediate variable MAX
+// Step 5: replace = with += operator, done
 
 'use strict';
 
@@ -9,7 +9,7 @@ const generatePassword = (alphabet, length) => {
   let key = '';
   for (let i = 0; i < length; i++) {
     const index = Math.floor(Math.random() * alphabet.length);
-    key = key + alphabet[index];
+    key += alphabet[index];
   }
   return key;
 };
