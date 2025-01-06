c6d03eca5a4ad09e9635dd53f0a03c1cd13ad216
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-06 17:11:14 +0200

    Add 'use strict' & const & let

---
+++ Tasks/between1.js
@@ -0,0 +1,24 @@
+// Add 'use strict';
+// Add `const` and `let`
+
+'use strict'
+
+const getvaluebetween = (str, p, s) => {
+  let i = str.indexOf(p);
+  if (i === -1) return "";
+  else {
+    const k = i + p.length;
+    str = str.substring(k);
+    if (s) {
+      i = str.indexOf(s);
+      if (i === -1) {
+        return "";
+      } else {
+        str = str.substring(0, i);
+      }
+    }
+  }
+  return str;
+};
+
+module.exports = getvaluebetween;
