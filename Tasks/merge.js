6efd829fdf06e79531b258c8370626f62d2f8568
Liza-Khube <elisekhu@gmail.com>
2025-01-06 17:07:09 +0200

    Replace for..in with spread operators (...)

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,13 +4,7 @@
 "use strict";
 
 const mergeObjects = (object1, object2) => {
-  const result = {};
-  for (const key in object1) {
-    result[key] = object1[key];
-  };
-  for (const key in object2) {
-    result[key] = object2[key];
-  };
+  const result = {...object1, ...object2};
   return result;
 };
 
