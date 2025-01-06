a4980240db3918dfb8302581608f7a38d17a320e
inryceu <pavel.maluev@gmail.com>
2025-01-06 22:46:34 +0200

    added strict + naming

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,7 +1,8 @@
 // Refactor following solution
 // Extract substring between prefix and suffix
+"use strict";
 
-getvaluebetween = (str, p, s) => {
+const getValueBetween = (str, p, s) => {
   i = str.indexOf(p);
   if (i === -1) return '';
   else {
@@ -19,4 +20,4 @@ getvaluebetween = (str, p, s) => {
   return str;
 };
 
-module.exports = getvaluebetween;
+module.exports = getValueBetween;
