ebb4dd4c45d7efdb55f3c6a4eab1106b649d4375
Luba <luba.chernii@gmail.com>
2025-01-06 22:17:03 +0100

    improved simplicity

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -1,11 +1,5 @@
 const duplicate = (value, N) => {
-  if (N <= 0) return [];
-
-  const result = [];
-  for (let i = 0; i < N; i++) {
-    result.push(value);
-  }
-  return result;
+  return N > 0 ? Array(N).fill(value) : [];
 };
 
 module.exports = duplicate;
