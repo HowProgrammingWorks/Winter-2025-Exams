3eae37df5cbf5a039bcf990ba135ba17ad3320c3
BalalaievMaxim <balalaev.maxim@lll.kpi.ua>
2025-01-06 23:47:46 +0200

    password.js: step 2

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,18 +1,18 @@
 // Refactor following solution
 // Generate random password
 
-// Step 1: added 'use strict', used prettier
+// Step 2: follow naming conventions
 
 'use strict';
 
-let GeneratePassword = (alphabet, length) => {
+let generatePassword = (alphabet, length) => {
   const MAX = alphabet.length;
   key = '';
   for (let i = 0; i < length; i++) {
-    Index = Math.floor(Math.random() * MAX);
-    key = key + alphabet[Index];
+    index = Math.floor(Math.random() * MAX);
+    key = key + alphabet[index];
   }
   return key;
 };
 
-module.exports = GeneratePassword;
+module.exports = generatePassword;
