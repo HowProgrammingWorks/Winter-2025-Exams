63c4e17bc541a9252d09a8fdc2c1f19dbfdd510d
Luba <luba.chernii@gmail.com>
2025-01-06 20:41:51 +0100

    improved reliability

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,10 +1,10 @@
-let compare = (firstDict, ...otherArgs) => {
+const compare = (firstDict, ...otherArgs) => {
   const secondDict = otherArgs[0];
-  let firstKeys = Object.keys(firstDict);
-  let secondKeys = Object.keys(secondDict);
+  const firstKeys = Object.keys(firstDict);
+  const secondKeys = Object.keys(secondDict);
   if (firstKeys.join('-') !== secondKeys.join('-')) return false;
   let areEqual = true;
-  for (let key of firstKeys) {
+  for (const key of firstKeys) {
     if (firstDict[key] !== secondDict[key]) {
       areEqual = false;
       break;
