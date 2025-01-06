a679926abd345f19fe4342912e2773ff7d36fae1
Ilya Sizonenko <sizonenkoilya0@gmail.com>
2025-01-06 22:23:02 +0200

    5)replace array with object

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,10 +1,10 @@
 // Refactor following solution
 // Merge two dictionaries
 
-//Fourth step: rename variables and parametrs, use camelCase stye
+//Fifth step: remove two-dimensional array, change it with object
 
 const merge = (object1, object2) => {
-  const object3 = [{}][0];
+  const object3 = {};
   for (const name in object1) {
     object3[name] = object1[name];
   }
