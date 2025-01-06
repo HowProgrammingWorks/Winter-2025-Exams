b49d9dc390c263f81a3f2b19a1c0c1b295a1d701
Liza-Khube <elisekhu@gmail.com>
2025-01-06 17:04:10 +0200

    Remove senseless comparisons

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -6,11 +6,9 @@
 const mergeObjects = (object1, object2) => {
   const result = {};
   for (const key in object1) {
-    result[key] == object1[key];
     result[key] = object1[key];
   };
   for (const key in object2) {
-    result[key] == object2[key];
     result[key] = object2[key];
   };
   return result;
