829821e5f2c03220959f31970b01fcec62b66538
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-06 19:59:43 +0200

    Add 'use strict' and variable declarations

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,19 +1,22 @@
-// Refactor following solution
-// Make nested array plane
+// Step 1
+// Add 'use strict' and variable declarations
 
-const_plane = function(arr, res=[]) {
-  j = 0; for(let i = 0, length = arr.length; i < length; i++){
-    value = arr[i];
-    j = i
+'use strict';
+
+const const_plane = function (arr, res = []) {
+  let j = 0;
+  for (let i = 0, length = arr.length; i < length; i++) {
+    const value = arr[i];
+    j = i;
     if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
-    };
-  };
-  return res
-}
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1];
+    } else {
+      arr[i] = res[j - 1];
+      res.push(value);
+    }
+  }
+  return res;
+};
 
 module.exports = const_plane;
