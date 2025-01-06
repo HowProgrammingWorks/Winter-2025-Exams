f91a5b8de4cc433883d419af69254776bd709d36
inryceu <pavel.maluev@gmail.com>
2025-01-07 00:09:08 +0200

    added strict, used set and array methods

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,19 +1,11 @@
 // Refactor following solution
 // Return an array without duplicates
+"use strict";
 
-DISTINCT = (data) => {
-  A = new Set();
-  w = 0;
-  data.forEach((a) => {
-    if (A.has(a)) {
-      delete data[w];
-    } else {
-      A.add(a);
-    }
-    w++;
-  });
-  return data.filter
-  (x => typeof x === 'number');
+const distinct = (data) => {
+  const set = new Set(data);
+  const result = Array.from(set);
+  return result;
 };
 
-module.exports = DISTINCT;
+module.exports = distinct;
