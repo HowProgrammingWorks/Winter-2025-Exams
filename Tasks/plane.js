1b4ebc78667ef1b890706ca62b20bd72a6bf2a75
Fersie_ <stas.oleksiichuk88@gmail.com>
2025-01-06 20:00:42 +0200

    Remove unnecessary parts

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,18 +1,17 @@
-// Step 1
-// Add 'use strict' and variable declarations
+// Step 2
+// Remove `j` declaration and using
+// Remove unnecessary `[i, length]` condition
+// Simplify conditions by removing `arr[i]` initialization
+// Remove `typeof value` from parameters of `.isArray()`
 
 'use strict';
 
 const const_plane = function (arr, res = []) {
-  let j = 0;
   for (let i = 0, length = arr.length; i < length; i++) {
     const value = arr[i];
-    j = i;
-    if (Array.isArray(value, typeof value) && [i, length]) {
+    if (Array.isArray(value)) {
       res.push(...const_plane(value));
-      arr[i] = res[i - 1];
     } else {
-      arr[i] = res[j - 1];
       res.push(value);
     }
   }
