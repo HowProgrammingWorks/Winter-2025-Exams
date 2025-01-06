6d8e5cc481ba709e3ea5487c7ae1b9186fbf238b
zolotonos <zolotonos.ua@gmail.com>
2025-01-06 23:08:32 +0200

    Simplify loop

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -6,16 +6,15 @@ const compareDictionaries = (dict1, dict2) => {
   const keys2 = Object.keys(dict2);
 
   if (keys1.join('-') !== keys2.join('-')) return false;
-  let areEqual = true;
+
   for (const key of keys1) {
-    if (dict1[key] === dict2[key]) {
-      areEqual = areEqual && true;
-    } else {
-      areEqual = areEqual && false;
+    if (dict1[key] !== dict2[key]) {
+      return false;
     }
   }
-  return areEqual;
+  return true;
 };
 
 module.exports = compareDictionaries;
+
 
