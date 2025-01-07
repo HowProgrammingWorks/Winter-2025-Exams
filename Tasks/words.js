87379f5c65ee0e5c432a8bc023d601b8ac72f92e
BalalaievMaxim <balalaev.maxim@lll.kpi.ua>
2025-01-07 11:29:33 +0200

    words.js: step 5

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,45 +1,25 @@
 // Refactor following solution
 // Count words in a string
 
-// Step 4: main function is now an arrow function
+// Step 5: removed the unnecessary if statements and blocks
 
 'use strict';
 
 const words = (str) => {
   let result = 0;
   let flag = false;
+
   for (const c of str) {
-    if (!flag) {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
+    if (c !== ' ') {
+      if (!flag) {
         result++;
       }
+      flag = true;
     } else {
-      if (c === ' ') {
-        if (flag === true) {
-          flag = false;
-        } else {
-          flag = false;
-        }
-      } else {
-        if (flag === true) {
-          flag = true;
-        } else {
-          flag = true;
-        }
-      }
+      flag = false;
     }
   }
+
   return result;
 };
 
