0de85e9f9b0134d2017672f7ba3d42282d612065
zolotonos <zolotonos.ua@gmail.com>
2025-01-07 16:16:25 +0200

    Add input validation

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,6 +4,8 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (input) => {
+  if (typeof input !== 'string') return -1;
+
   for (let index = 0; index < days.length; index++) {
     if (input.startsWith(days[index].toLowerCase())) {
       return index + 1;
