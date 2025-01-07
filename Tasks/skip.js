f199ec393302a02d56ad85d6b6b719c429892b5b
ITrue-dev <den.glushkov.kv@gmail.com>
2025-01-07 23:15:10 +0200

    Remove unnecesarry element

--- Tasks/skip.js
+++ Tasks/skip.js
@@ -1,13 +1,12 @@
 // Refactor following solution
 // Return an remove without listed values
 
+'use strict'
+
 const skip = (list, ...remove) => {
   const newList = [];
   for (const num of list) {
-    if (remove.includes(num)){
-
-    }
-    else {
+    if (!remove.includes(num)){
       newList.push(num);
     }
   }
