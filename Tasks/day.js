14c99ff7f63338591d0c20acaeb6b245446059cf
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 01:54:20 +0200

    day - changed naming of the function and deleted the comments

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,11 +4,11 @@
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const parseDay = (dayString) => {
+const dayNumber = (dayString) => {
   if (typeof dayString !== 'string') return -1;
 
   const index = days.findIndex(day => dayString.startsWith(day.toLowerCase()));
   return index === -1 ? -1 : index + 1;
 };
 
-module.exports = parseDay;
+module.exports = dayNumber;
