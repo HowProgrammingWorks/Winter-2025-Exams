950132699ab9a24070487e80f4984e2199f8d609
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 01:48:38 +0200

    day - adding arrays instead of the loops

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,14 +4,9 @@
 
 let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-parseDay = (dayString) => {
-  let i;
-  for (i = 0; i < days.length; i++) {
-    if (dayString.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
-  }
-  return -1;
+const parseDay = (dayString) => {
+  const index = days.findIndex(day => dayString.startsWith(days[i].toLowerCase()));
+  return index === -1 ? -1 : index + 1;
 };
 
 module.exports = parseDay;
