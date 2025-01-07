ff03014cf1955b39d5c7545f64f049861e7fdc56
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2025-01-08 01:05:17 +0200

    Step 5: remove flag usage

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,9 +1,8 @@
 // Refactor following solution
 // Compare two dictionaries
 
-// Step 4:
-// Remove senseless else statement and invert condition
-// Simplify isEqual assignment
+// Step 5:
+// Remove flag usage
 
 "use strict";
 
@@ -13,11 +12,10 @@ const compare = (object1, object2) => {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
   if (keys1.join(SEPARATOR) !== keys2.join(SEPARATOR)) return false;
-  let isEqual = true;
   for (const key of keys1) {
-    if (object1[key] !== object2[key]) isEqual = true;
+    if (object1[key] !== object2[key]) return false;
   }
-  return isEqual;
+  return true;
 };
 
 module.exports = compare;
