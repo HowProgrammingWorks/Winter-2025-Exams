083029873317170241624952ace6163f9134cdaa
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 17:22:36 +0200

    Add 'use strict' and optimization

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,14 +1,16 @@
 // Refactor following solution
 // Compare two dictionaries
 
-let compare = (first_values, ...parameters_LIST) => {
+'use strict'
+
+const compare = (first_values, ...parameters_LIST) => {
   const second_values = parameters_LIST[0];
-  let a = Object.keys(first_values);
-  let b = Object.keys(second_values);
+  const a = Object.keys(first_values);
+  const b = Object.keys(second_values);
   if (a.join('-') !== b.join('-')) return false;
   let e = true;
-  for (c of a) {
-    if (first_values[c] === second_values[c]) e = e && true;
+  for (const key of a) {
+    if (first_values[key] === second_values[key]) e = e && true;
     else {
       e = e && false;
     }
