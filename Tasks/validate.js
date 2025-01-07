36184b57784cf58025971f36919103bbc0106e31
BalalaievMaxim <balalaev.maxim@lll.kpi.ua>
2025-01-07 12:13:57 +0200

    validate.js: step 4

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,16 +1,15 @@
 // Refactor following solution
 // Validate person name
 
-// Step 3: char is now defined correctly
+// Step 4: if statements replaced with single one, with cleanup of duplicates
 
 'use strict';
 
 const validate = (name) => {
-  if (!name) return false;
-  if (name === '') return false;
-  if (typeof name !== 'string') return false;
-  if (name.length === 0) return false;
-  if (!name.includes(' ')) return false;
+  if (!name ||
+    typeof name !== 'string' ||
+    !name.includes(' ')) return false;
+
   {
     for (const char of name) {
       if (char === ' ') continue;
