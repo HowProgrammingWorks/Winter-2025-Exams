4f8e65802acd3653c7d768247c2f6e50642a00ab
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 23:58:53 +0200

    Use spread operator

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,14 +4,7 @@
 'use strict'
 
 const twoMergedObjects = (firstObject, secondObject) => {
-  const resultObject = {};
-  for (const attributeName in firstObject) {
-    resultObject[attributeName] = firstObject[attributeName];
-  }
-  for (const attributeName in secondObject) {
-    resultObject[attributeName] = secondObject[attributeName];
-  }
-  return resultObject;
+  return {...firstObject, ...secondObject};
 };
 
 module.exports = twoMergedObjects;
