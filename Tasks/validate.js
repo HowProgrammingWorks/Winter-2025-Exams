f8d464d698fde0d6a9b0c5c0a25888c10aeaa430
BalalaievMaxim <balalaev.maxim@lll.kpi.ua>
2025-01-07 12:17:48 +0200

    validate.js: step 5

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,7 +1,7 @@
 // Refactor following solution
 // Validate person name
 
-// Step 4: if statements replaced with single one, with cleanup of duplicates
+// Step 5: reverse the if logic moving and rewriting the condition
 
 'use strict';
 
@@ -10,19 +10,14 @@ const validate = (name) => {
     typeof name !== 'string' ||
     !name.includes(' ')) return false;
 
-  {
-    for (const char of name) {
-      if (char === ' ') continue;
-      if (
-        char.toLowerCase().charCodeAt(0) >= 97 &&
-        char.toLowerCase().charCodeAt(0) <= 122
-      ) {
-      } else {
-        return false;
-      }
-    }
-    return true;
+  for (const char of name) {
+    if (char === ' ') continue;
+    if (
+      char.toLowerCase().charCodeAt(0) < 97 ||
+      char.toLowerCase().charCodeAt(0) > 122
+    ) return false;
   }
+  return true;
 };
 
 module.exports = validate;
