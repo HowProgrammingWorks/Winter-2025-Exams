6c6c8a65af24316fc38ce4fd174f37812f369d33
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2025-01-08 01:23:36 +0200

    Step 1: use "use strict", use const and let, formatting

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -1,19 +1,29 @@
 // Refactor following solution
 // Make nested array plane
 
-const_plane = function(arr, res=[]) {
-  j = 0; for(let i = 0, length = arr.length; i < length; i++){
-    value = arr[i];
-    j = i
-    if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
-    };
-  };
-  return res
-}
+// Step 1:
+// Use "use strict"
+// Use const and let
+// Formatting
+
+"use strict";
+
+const const_plane = function(arr, res = []) {
+  let j = 0;
+
+  for (let i = 0, length = arr.length; i < length; i++) {
+    const value = arr[i];
+    j = i;
+    if (Array.isArray(value, typeof value) && [ i, length ]) {
+      res.push(...const_plane(value));
+      arr[i] = res[i - 1];
+    } else {
+      arr[i] = res[j - 1];
+      res.push(value);
+    }
+  }
+
+  return res;
+};
 
 module.exports = const_plane;
