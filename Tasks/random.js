4da36dd21181c1abe4d21e62e9e9681066694df7
Ann0123-cyber <anna.musiiovska@gmail.com>
2025-01-08 01:51:32 +0200

    added use strict and deleted useless code

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,13 +1,9 @@
 // Refactor following solution
 // Generate random integer value in given range
+'use strict';
 
 Random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+  return min + Math.floor(Math.random() * (max - min + 1));
 };
 
 module.exports = Random;
