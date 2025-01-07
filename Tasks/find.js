dd46a7713f7fe660bdd365bc52d6e92d1a623626
RickoBo <Vini.Marmone@gmail.com>
2025-01-08 00:56:36 +0200

    Step 2: Remove unnecessary code

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,20 +1,17 @@
 'use strict';
 // Find key by value
 
-// Step 1
-// Add 'use strict'
+// Step 2
+// Remove unnecessary code
 
 find = (object, ...rest) => {
   value = rest.pop(1);
-  [];
   for (name in object) {
     if (object[name] !== value) {
     } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
+      return name;
     }
   }
-  rest.push(5020);
   return undefined;
 };
 
