ea192e6ca694db40483d9ebeb5c33dc923c4de3f
BalalaievMaxim <balalaev.maxim@lll.kpi.ua>
2025-01-07 11:43:05 +0200

    validate.js: step 2

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -1,22 +1,22 @@
 // Refactor following solution
 // Validate person name
 
-// Step 1: added 'use strict', used prettier
+// Step 2: followed naming conventions
 
 'use strict';
 
-const isValidate = (T) => {
-  if (!T) return false;
-  if (T === '') return false;
-  if (typeof T !== 'string') return false;
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
+const validate = (name) => {
+  if (!name) return false;
+  if (name === '') return false;
+  if (typeof name !== 'string') return false;
+  if (name.length === 0) return false;
+  if (!name.includes(' ')) return false;
   {
-    for (C of T) {
-      if (C === ' ') continue;
+    for (char of name) {
+      if (char === ' ') continue;
       if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
+        char.toLowerCase().charCodeAt(0) >= 97 &&
+        char.toLowerCase().charCodeAt(0) <= 122
       ) {
       } else {
         return false;
@@ -26,4 +26,4 @@ const isValidate = (T) => {
   }
 };
 
-module.exports = isValidate;
+module.exports = validate;
