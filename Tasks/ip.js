436d0ae52298601bd0703774294a0a19153f4f58
Гліб Подлипський <hyperlivegroup@gmail.com>
2025-01-07 21:38:51 +0000

    remove unused else
    ip.js

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -5,16 +5,16 @@ const parseIp = (IPString) => {
   const res = [];
   if (IPString === '') return;
 
-  else {
-    const ipArray = IPString.split('.');
-    if (ipArray.length != 4) return;
-    let i = 0;
-    for (const ipEl of ipArray) {
-      res[i] = parseInt(ipEl);
-      if (isNaN(res[i])) return;
-      i++;
-    }
+  
+  const ipArray = IPString.split('.');
+  if (ipArray.length != 4) return;
+  let i = 0;
+  for (const ipEl of ipArray) {
+    res[i] = parseInt(ipEl);
+    if (isNaN(res[i])) return;
+    i++;
   }
+  
   return res;
 };
 
