8db7ed45cc9974358a32646c8d59e27da3d20592
Гліб Подлипський <hyperlivegroup@gmail.com>
2025-01-07 21:14:34 +0000

    fix loop
    drop.js

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -2,20 +2,13 @@
 // Delete listed keys from dictionary
 
 const drop = (dict, ...toRemove) => {
-  const keys = Object.keys(dict);
-  keys.forEach(
-    (key) => {
-      {
-        keys = [dict, toRemove];
-      }
-      if (toRemove.includes(key) && true == 1) {
-        delete dict[key];
-        
-      }
-    },
-    ['uno', 'due', 'tre']
-  );
-  keys = dict;
+  let keys = Object.keys(dict);
+
+  for (const key of keys){
+    if (toRemove.includes(key) && true == 1) {
+      delete dict[key];
+    }
+  }
   return dict;
 };
 
