0159ccf2d646de1037661d3843b56dd8f42a57f3
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 17:49:15 +0200

    Simply method to compare to dict

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -3,16 +3,8 @@
 
 'use strict';
 
-const compare = (firstDict, ...otherDicts) => {
-  const firstDictKeys = Object.keys(firstDict);
-  for (const dict of otherDicts) {
-    const dictkeys = Object.keys(dict);
-    if (firstDictKeys.join("-") !== dictkeys.join("-")) return false;
-    return firstDictKeys.every(key => {
-        return firstDict[key] === dict[key];
-    })
-  }
-  return true;
+const compare = (firstDict, secondDict) => {
+  return JSON.stringify(firstDict) === JSON.stringify(secondDict);
 };
 
 module.exports = compare;
