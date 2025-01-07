a23119c5065d86391523af2d8137a2b6b47cca7d
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-01-07 12:36:13 +0200

    step 5: rename variables

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,23 +1,23 @@
 // Refactor following solution
 // Sum all numbers from an array
 
-// step 4: add const declarations
+// step 5: rename variables
 
 'use strict';
 
 const sum = (array) => {
   let res = 0;
 
-  for (const i of s) {
-    const t = typeof i;
-    if (t === 'number') {
-      if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
-        sum.push(new_Sum);
+  for (const elem of array) {
+    const type = typeof elem;
+    if (type === 'number') {
+      if (array.length > 0) {
+        const newSum = array[array.length - 1] + elem;
+        array.push(newSum);
       }
     }
   }
-  return sum[sum.length - 1];
+  return array[array.length - 1];
 };
 
 module.exports = sum;
