69209f01f8c672111c33d8fdbe3468ea965b64b0
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 20:15:59 +0200

    Use method .find and ternary operator

--- Tasks/find.js
+++ Tasks/find.js
@@ -5,10 +5,8 @@
 
 const findInObject = (object, checkValues) => {
   const pairsKeyValue = Object.entries(object)
-  for (const pair of pairsKeyValue) {
-      if (pair[1] === checkValues) return pair[0];
-  }
-  return undefined;
+  const foundElement = pairsKeyValue.find(element => element[1] === checkValues)
+  return foundElement !== -1 ? foundElement[0] : undefined;
 };
 
 module.exports = findInObject;
