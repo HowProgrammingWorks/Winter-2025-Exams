a09f1e20a78127edea94560544652ff581f76852
Mykola Semenenko <mikolasemenenko5@gmail.com>
2025-01-08 00:35:11 +0200

    Add 'let'

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,13 +1,13 @@
 // Refactor following solution
 // Sum all number values in dict
 
-//Step 2
-//Remove unneeded parenthesis, brackets and braces
+//Step 3
+//Add `let`
 
 'use strict';
 
 count = (obj) => {
-  sum = 0;
+  let sum = 0;
   keys = Object.keys(obj);
   keys.forEach((key) => {
     const value = obj[key];
