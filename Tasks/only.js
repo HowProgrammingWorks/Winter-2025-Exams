273be733b3fec3d2af0343130fef8ce511270291
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-01-07 12:50:27 +0200

    step 6: prevent modification of input object in function

--- Tasks/only.js
+++ Tasks/only.js
@@ -1,18 +1,20 @@
 // Refactor following solution
 // Copy only listed values from dict
 
-// step 5: replace forEach with for...of loop
+// step 6: prevent modification of input object in function
 
 'use strict';
 
 const only = (dict, ...listedValue) => {
+  const result = {};
+
   const keys = Object.keys(dict);
 
   for (const key of keys) {
-    if (!listedValue.includes(key)) delete dict[key];
+    if (listedValue.includes(key)) result[key] = dict[key];
   }
 
-  return dict;
+  return result;
 };
 
 module.exports = only;
