adc8d288564b86c474ab5b79ed1c8d4d98fe71e2
LobanMihajlo <loban.mihajlo2411@gmail.com>
2025-01-07 17:49:48 +0200

    Step 3: remove unnecessary if-else block

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,20 +1,17 @@
 // Refactor following solution
 // Split string by the first occurrence of separator
 
-'use strict'
+'use strict';
 
 const Parseip = (ip) => {
   const res = [];
-  if (ip === '') return;
-  else {
-    const array = ip.split('.');
-    if (array.length != 4) return;
-    let j = 0;
-    for (const el of array) {
-      res[j] = parseInt(el);
-      if (isNaN(res[j])) return;
-      j++;
-    }
+  const array = ip.split('.');
+  if (array.length != 4) return;
+  let j = 0;
+  for (const el of array) {
+    res[j] = parseInt(el);
+    if (isNaN(res[j])) return;
+    j++;
   }
   return res;
 };
