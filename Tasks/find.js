78585a039c345175d43c756aef66d49e62a1370d
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-01-07 12:58:54 +0200

    step 3: remove unnecessary code block

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,21 +1,19 @@
 // Refactor following solution
 // Find key by value
 
-// step 2: add const declarations
+// step 3: remove unnecessary code block
 
 'use strict';
 
 const find = (object, ...rest) => {
-  const value = rest.pop(1);
-  [];
+  const value = rest.pop();
+
   for (const name in object) {
-    if (object[name] !== value) {
-    } else {
-      if (typeof name) return name;
-      if (typeof object) return object;
+    if (object[name] === value) {
+      return name;
     }
   }
-  rest.push(5020);
+
   return undefined;
 };
 
