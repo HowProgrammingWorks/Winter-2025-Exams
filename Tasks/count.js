a805933b9f69afcbdc135382d5281cb839318966
Mykola Semenenko <mikolasemenenko5@gmail.com>
2025-01-08 00:44:20 +0200

    Add const; Remove unnecessary semicolon; Use for..of instead of forEach

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,18 +1,20 @@
 // Refactor following solution
 // Sum all number values in dict
 
-//Step 3
-//Add `let`
+//Step 4
+//Add `const`
+//Remove unnecessary semicolon
+//Use for..of instead of forEach
 
 'use strict';
 
-count = (obj) => {
+const count = (obj) => {
   let sum = 0;
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
+  const keys = Object.keys(obj);
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
-  });
+  }
 
   return sum;
 };
