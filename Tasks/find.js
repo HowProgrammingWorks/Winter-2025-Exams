626ca990075b9ca8161e97b78611840c426cf3c4
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-01-07 12:58:10 +0200

    step 2: add const declarations

--- Tasks/find.js
+++ Tasks/find.js
@@ -1,14 +1,14 @@
 // Refactor following solution
 // Find key by value
 
-// step 1: add 'use strict'
+// step 2: add const declarations
 
 'use strict';
 
-find = (object, ...rest) => {
-  value = rest.pop(1);
+const find = (object, ...rest) => {
+  const value = rest.pop(1);
   [];
-  for (name in object) {
+  for (const name in object) {
     if (object[name] !== value) {
     } else {
       if (typeof name) return name;
