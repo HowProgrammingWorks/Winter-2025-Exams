f02e8ef620cfb01949f38a2b08bb3e5eb0267f17
M1RVKL <bielov.vlad@gmail.com>
2025-01-07 15:11:55 +0200

    used Array.fill to shorten the code, defined a function

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,16 +1,17 @@
 // Refactor following solution
 // Find an intersection of two dictionaries
 'use strict'
-function intersection(object_1, object_2) {
-  let firstKeys = Object.keys(object_1);
+
+function intersection(object1, object2) {
+  let firstKeys = Object.keys(object1);
   for (let attributeName of firstKeys) {
-    if (object_1[attributeName] === object_2[attributeName]) {
-      object_2[attributeName] = object_1[attributeName];
+    if ( object1[attributeName] === object2[attributeName]) {
+      object2[attributeName] = object1[attributeName];
     } else {
-      delete object_1[attributeName];
+      delete object1[attributeName];
     }
   }
-  return object_1;
+  return object1;
 };
 
 module.exports = intersection;
