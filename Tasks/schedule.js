4f1922f3c2d50422cf18d626438271d417b0e1ae
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 21:19:16 +0200

    Implement following task

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -3,6 +3,10 @@
 // Implement following task
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
 
-const schedule = async (func, n, interval) => {};
+const schedule = async (func, n, interval) => {
+    for(let i = 1; i <= n; i++) {
+        setTimeout(func, interval, n)
+    }
+};
 
 module.exports = schedule;
