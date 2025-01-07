45d6cf1961b5e3be102ed1efefe04cfc5fa3de0d
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-01-07 12:14:59 +0200

    step 5: move declaration of let i inside the loop

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,15 +1,14 @@
 // Refactor following solution
 // Get day number
 
-// step 4: rename data in array and delete toLowerCase() for easier index search
+// step 5: move declaration of let i inside the loop
 
 'use strict';
 
 const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const getDayNumber = (str) => {
-  let i;
-  for (i = 0; i < DAYS.length; i++) {
+  for (let i = 0; i < DAYS.length; i++) {
     if (str.startsWith(DAYS[i])) {
       return i + 1;
     }
