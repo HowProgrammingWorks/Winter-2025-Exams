1eb9db2e14ac7063b2c4d9e3163389bba5837f7f
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 19:52:29 +0200

    Don't mutate incoming parameters

--- Tasks/except.js
+++ Tasks/except.js
@@ -5,12 +5,13 @@
 
 const expect = (dict, ...stringList) => {
   const keys = Object.keys(dict);
+  const newDict = {};
   keys.forEach((key) => {
-    if (stringList.includes(key)) {
-      delete dict[key];
+    if (!stringList.includes(key)) {
+      newDict[key] = dict[key];
     }
   });
-  return dict;
+  return newDict;
 };
 
 module.exports = expect;
