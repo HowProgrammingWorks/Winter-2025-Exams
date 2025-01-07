6027ac00aab636c90dc3f0c0c7b2795cff6366cf
RickoBo <Vini.Marmone@gmail.com>
2025-01-07 02:21:31 +0200

    Step 4: change function name, remove if statement

--- Tasks/range.js
+++ Tasks/range.js
@@ -1,20 +1,16 @@
 'use strict';
 // Generate int array from given range
 
-// Step 3
-// Change parameters
-// Declare array 'result'
-// Replace variable 'from' and 'to' with 'first' and 'last'
-// Remove unnecessary code
+// Step 4
+// Rename function
+// Remove if statement
 
-const _range = (first, last) => {
+const range = (first, last) => {
   let result = [];
-  if (last >= first) {
-    for (let i = first; i <= last; i++) {
-      result[i - first] = i;
-    }
+  for (let i = first; i <= last; i++) {
+    result[i - first] = i;
   }
   return result;
 };
 
-module.exports = _range;
+module.exports = range;
