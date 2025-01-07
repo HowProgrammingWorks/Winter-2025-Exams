9fcfe6da0109bcf480763ab24d5fb30310ece472
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 20:32:10 +0200

    Simplify logic and delete unnecessary rewrite of key and value

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -6,10 +6,8 @@
 const intersection = (firstObject, secondObject) => {
   const firstKeys = Object.keys(firstObject);
   for (const attributeName of firstKeys) {
-    if (firstObject[attributeName] === secondObject[attributeName]) {
-      secondObject[attributeName] = firstObject[attributeName];
-    } else {
-      delete firstObject[attributeName];
+    if (firstObject[attributeName] !== secondObject[attributeName]) {
+      delete firstObject[attributeName]
     }
   }
   return firstObject;
