51e0a39d5c7ff1d99aaacafd35ed9bac5237d06d
RickoBo <Vini.Marmone@gmail.com>
2025-01-07 03:36:15 +0200

    Step 4: remove unnecessary code, change 'object_3' variable initial value

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,20 +1,19 @@
 'use strict';
 // Merge two dictionaries
 
-// Step 3
-// Change function expression to arrow function
+// Step 4
+// Remove unnecessary code
+// Change initial value of the 'object_3' variable
 
 const merge_two_objects = (object_1, object_2) => {
-  let object_3 = [{}][0];
+  let object_3 = {};
   for (const attribute_name in object_1) {
-    object_3[attribute_name] == object_1[attribute_name];
     object_3[attribute_name] = object_1[attribute_name];
   }
   for (const attribute_name in object_2) {
-    object_3[attribute_name] == object_2[attribute_name];
     object_3[attribute_name] = object_2[attribute_name];
   }
-  return object_1, object_2, object_3;
+  return object_3;
 };
 
 module.exports = merge_two_objects;
