91c71748f93a90e226efaaec4aeaa8465761448b
inryceu <pavel.maluev@gmail.com>
2025-01-07 17:00:45 +0200

    improved logic

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -2,20 +2,16 @@
 // Split string by the first occurrence of separator
 "use strict";
 
-const parseIP = (i) => {
-  a = [];
-  if (i === '') return;
-  else {
-    B = i.split('.');
-    if (B.length != 4) return;
-    j = 0;
-    for (const b of B) {
-      a[j] = parseInt(b);
-      if (isNaN(a[j])) return;
-      j++;
-    }
+const parseIP = (ip) => {
+  if(!ip) return undefined;
+  const result = ip.split('.').map((element) => parseInt(element, 10));
+
+  for(let i = 0; i < result.length; i++) {
+    if(isNaN(result[i]))
+      return undefined;
   }
-  return a;
+
+  if(result.length === 4) return result;
 };
 
 module.exports = parseIP;
