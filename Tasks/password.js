71e0ef1262eda04d5e7d8e2c88aba8525aafbeaa
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2025-01-08 01:20:32 +0200

    Step 3: simplify password assignment, formatting

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,19 +1,21 @@
 // Refactor following solution
 // Generate random password
 
-// Step 2:
-// Fix and Use "use strict"
-// Rename variable using camel case
+// Step 3:
+// Simplify password assignment
+// Formatting
 
 "use strict";
 
 const generatePassword = (alphabet, length) => {
   const max = alphabet.length;
-  let password = '';
+  let password = "";
+
   for (let i = 0; i < length; i++) {
     const index = Math.floor(Math.random() * max);
-    password = password + alphabet[index];
+    password += alphabet[index];
   }
+
   return password;
 };
 
