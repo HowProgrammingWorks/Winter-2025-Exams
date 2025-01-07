205bc774ba6c8396d9b3ed2d105549fc1a21f5c3
inryceu <pavel.maluev@gmail.com>
2025-01-07 19:38:59 +0200

    added style

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -3,19 +3,20 @@
 "use strict";
 
 const constPlane = (arr) => {
-  j = 0; 
-  for(let i = 0, length = arr.length; i < length; i++){
-    value = arr[i];
-    j = i
-    if (Array.isArray(value, typeof value) && [i, length]) {
-    res.push(...const_plane(value));
-    arr[i] = res[i - 1]
-    }else{
-    arr[i] = res[j - 1]
-    res.push(value);
-    };
-  };
-  return res
+  let res = [];
+  let j = 0; 
+  for(let i = 0; i < arr.length; i++) {
+    let value = arr[i];
+    j = i;
+    if (Array.isArray(value, typeof value)) {
+      res.push(...constPlane(value));
+      arr[i] = res[i - 1];
+    } else {
+      arr[i] = res[j - 1];
+      res.push(value);
+    }
+  }
+  return res;
 }
 
 module.exports = constPlane;
