d638cb7a90ec9e78d6fe05b0d4c9bfc5f480c099
ITrue-dev <den.glushkov.kv@gmail.com>
2025-01-07 21:14:37 +0200

    Remove method pop()

--- Tasks/find.js
+++ Tasks/find.js
@@ -4,14 +4,13 @@
 'use strict'
 
 const find = (object, ...rest) => {
-  const value = rest.pop();
+  const value = rest[0];
 
   for (const [name, objValue] of Object.entries(object)) {
     if (objValue === value) {
       return name;
     }
-  }
-  
+  }  
 };
 
 module.exports = find;
