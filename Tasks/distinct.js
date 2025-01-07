50c6f574c9bfae20d9803ac2258f08f97b4b0442
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 18:21:13 +0200

    Remove filter

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -7,12 +7,12 @@ const distinct = (data) => {
   const uniqueCollection = new Set();
   const uniqueData = [];
   data.forEach((element) => {
-    if (!uniqueCollection.has(element)) {
+    if (!uniqueCollection.has(element) && typeof element === 'number') {
       uniqueCollection.add(element);
       uniqueData.push(element);
     }
   });
-  return uniqueData.filter((x) => typeof x === 'number');
+  return uniqueData;
 };
 
 module.exports = distinct;
