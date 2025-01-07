0f48f1a61eb0aac2e70c928e6124ad597f7704a2
Mykola Semenenko <mikolasemenenko5@gmail.com>
2025-01-08 00:31:57 +0200

    Remove unneeded parenthesis, brackets and braces.

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,22 +1,19 @@
 // Refactor following solution
 // Sum all number values in dict
 
-//Step 1
-//Add 'use strict';
+//Step 2
+//Remove unneeded parenthesis, brackets and braces
 
 'use strict';
 
 count = (obj) => {
   sum = 0;
-  {
-  }
   keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
+
   return sum;
 };
 
