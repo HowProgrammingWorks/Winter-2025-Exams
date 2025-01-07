da0ccfb6901b180f631e105d5052554953ccb3ae
Гліб Подлипський <hyperlivegroup@gmail.com>
2025-01-07 21:21:35 +0000

    make code prettier
    filter.js

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -3,13 +3,17 @@
 
 const filter = (array, typeName) => {
   const remove = [];
+
   for (const el of array) {
     const index = array.indexOf(el);
     if (typeof array[index] !== typeName) {
       remove.unshift(index);
     }
   }
-  for (constindex of remove) array.splice(index, 1);
+
+  for (const index of remove){
+     array.splice(index, 1);
+  }
   return array;
 };
 
