c44d31a7f4c975303ab35b3d204020ea5a0c2a77
RickoBo <Vini.Marmone@gmail.com>
2025-01-08 00:23:09 +0200

    Step 2: add const and let

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,13 +1,12 @@
 'use strict';
 // Sum all number values in dict
 
-// Step 1
-// Add 'use strict';
-// Remove empty blocks
+// Step 2
+// Add const and let
 
-count = (obj) => {
-  sum = 0;
-  keys = Object.keys(obj);
+const count = (obj) => {
+  let sum = 0;
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
