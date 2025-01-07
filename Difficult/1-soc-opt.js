5fa5dcaffecefb801629c54290f5d782ad0caaa7
Oleksandr Cherepov <sashacherepov55@gmail.com>
2025-01-07 17:48:08 +0200

    Refactor: divide algorithm into different functions

--- Difficult/1-soc-opt.js
+++ Difficult/1-soc-opt.js
@@ -22,27 +22,51 @@ const data = `city,population,area,density,country
   New York City,8537673,784,10892,United States
   Bangkok,8280925,1569,5279,Thailand`;
 
-if (data) {
+const formTable = (data) => {
   const lines = data.split('\n');
   lines.pop();
   const table = [];
   let first = true;
-  let max = 0;
   for (const line of lines) {
     if (first) {
       first = false;
     } else {
       const cells = line.split(',');
-      const d = parseInt(cells[3]);
-      if (d > max) max = d;
       table.push([cells[0], cells[1], cells[2], cells[3], cells[4]]);
     }
   }
+  return table;
+};
+
+const getMax = (table) => {
+  let max = 0;
+  for (const row of table) {
+    const d = parseInt(row[3]);
+    if (d > max) max = d;
+  }
+  return max;
+};
+
+const sortTable = (table) => {
+  const max = getMax(table);
   for (const row of table) {
     const a = Math.round((row[3] * 100) / max);
     row.push(a.toString());
   }
   table.sort((r1, r2) => r2[5] - r1[5]);
+  return table;
+};
+
+const createTable = (data) => {
+  let sortedTable = undefined;
+  if (data) {
+    const table = formTable(data);
+    sortedTable = sortTable(table);
+  }
+  return sortedTable;
+};
+
+const logTable = (table) => {
   for (const row of table) {
     let s = row[0].padEnd(18);
     s += row[1].padStart(10);
@@ -52,4 +76,6 @@ if (data) {
     s += row[5].padStart(6);
     console.log(s);
   }
-}
+};
+
+logTable(createTable(data));
