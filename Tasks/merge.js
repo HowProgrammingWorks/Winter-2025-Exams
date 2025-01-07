66028940225c09ea7a2161fa9a311cbda8ba1515
inryceu <pavel.maluev@gmail.com>
2025-01-07 17:16:53 +0200

    improved logic, avoided data mutability

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -3,16 +3,14 @@
 "use strict";
 
 const mergeTwoObjects = (obj1, obj2) => {
-  object_3 = {};
-  for (attribute_name in obj1) {
-    object_3[attribute_name] == obj1[attribute_name];
-    object_3[attribute_name] = obj1[attribute_name];
-  }
-  for (attribute_name in obj2) {
-    object_3[attribute_name] == obj2[attribute_name];
-    object_3[attribute_name] = obj2[attribute_name];
-  }
-  return object_3;
+  let result = {};
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  for(let key of keys1)
+    result[key] = obj1[key];
+  for(let key of keys2)
+    result[key] = obj2[key];
+  return result;
 };
 
 module.exports = mergeTwoObjects;
