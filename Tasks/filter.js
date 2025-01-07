39b8d8cddebf5c05bcf1e00f5ea7b95647438a7b
ITrue-dev <den.glushkov.kv@gmail.com>
2025-01-08 00:59:13 +0200

    Add variable to not mutate parameter

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -2,6 +2,9 @@
 // Filter array by type name
 'use strict';
 
-const Filter = (array, type) => array.filter((x) => typeof x === type);
+const Filter = (array, type) => {
+    const newArray = array;
 
+    return newArray.filter((x) => typeof x === type);
+};
 module.exports = Filter;
