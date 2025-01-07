3dfb8db2a6df45915af171b7f600df9cc23b2987
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 20:30:51 +0200

    Remove unnecessary code, rename variables and use arrow function

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,26 +3,16 @@
 
 'use strict'
 
-const intersection = function intersection(object_1, object_2) {
-  const first_keys = Object.keys(object_1);
-  {
-  }
-  for (const attribute_name of first_keys) {
-    if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
-      {
-      }
+const intersection = (firstObject, secondObject) => {
+  const firstKeys = Object.keys(firstObject);
+  for (const attributeName of firstKeys) {
+    if (firstObject[attributeName] === secondObject[attributeName]) {
+      secondObject[attributeName] = firstObject[attributeName];
     } else {
-      delete object_1[attribute_name];
-      {
-      }
-    }
-    {
+      delete firstObject[attributeName];
     }
   }
-  {
-  }
-  return object_1;
+  return firstObject;
 };
 
 module.exports = intersection;
