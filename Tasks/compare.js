cd677b1ab28ebd37114b99140a18c3fa43c8b4f5
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2025-01-08 01:06:30 +0200

    Step 6: formatting

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,20 +1,23 @@
 // Refactor following solution
 // Compare two dictionaries
 
-// Step 5:
-// Remove flag usage
+// Step 6:
+// Formatting
 
 "use strict";
 
-const SEPARATOR = '-';
+const SEPARATOR = "-";
 
 const compare = (object1, object2) => {
-  const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2);
+  const keys1 = Object.keys(object1),
+    keys2 = Object.keys(object2);
+
   if (keys1.join(SEPARATOR) !== keys2.join(SEPARATOR)) return false;
+
   for (const key of keys1) {
     if (object1[key] !== object2[key]) return false;
   }
+
   return true;
 };
 
