44f673578d98eccd6427da75c677c5978d4c491e
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-08 00:14:44 +0200

    Using array method .findIndex

--- Tasks/find.js
+++ Tasks/find.js
@@ -4,9 +4,10 @@
 'use strict'
 
 const findInObject = (object, checkValues) => {
-  const pairsKeyValue = Object.entries(object)
-  const foundElement = pairsKeyValue.find(element => element[1] === checkValues)
-  return foundElement !== -1 ? foundElement[0] : undefined;
+  const keys = Object.keys(object)
+  const values = Object.values(object)
+  const foundIndex = values.findIndex(element => element === checkValues)
+  return foundIndex !== -1 ? keys[foundIndex] : undefined;
 };
 
 module.exports = findInObject;
