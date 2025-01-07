3540aab424d9ebb02247203c636809565c0c1a99
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-07 17:33:44 +0200

    Realization if only two dictionaries can be in function, delete unused code

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -3,19 +3,16 @@
 
 'use strict'
 
-const compare = (firstDict, ...otherDict) => {
-  const secondDict = otherDict[0];
-  const firstDictKeys = Object.keys(firstDict);
-  const secondDictKeys = Object.keys(secondDict);
-  if (firstDictKeys.join('-') !== secondDictKeys.join('-')) return false;
-  let flag = true;
-  for (const key of firstDictKeys) {
-    if (firstDict[key] === secondDict[key]) flag = flag && true;
-    else {
-      flag = flag && false;
+const compare = (firstDict, secondDict) => {
+    const firstDictKeys = Object.keys(firstDict);
+    const secondDictKeys = Object.keys(secondDict);
+    if (firstDictKeys.join('-') !== secondDictKeys.join('-')) return false;
+    for (const key of firstDictKeys) {
+      if (firstDict[key] !== secondDict[key]) {
+        return false;
+      };
     }
-  }
-  return flag;
-};
-
-module.exports = compare;
+    return true;
+  };
+  
+  module.exports = compare;
