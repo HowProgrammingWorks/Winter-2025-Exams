857fb52a5c5a2706addcae8f739ca822dda2d90a
RickoBo <Vini.Marmone@gmail.com>
2025-01-07 03:31:01 +0200

    Step 3: change function expression to arrow function

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,10 +1,10 @@
 'use strict';
 // Merge two dictionaries
 
-// Step 2
-// Add const and let
+// Step 3
+// Change function expression to arrow function
 
-const merge_two_objects = function merge_two_objects(object_1, object_2) {
+const merge_two_objects = (object_1, object_2) => {
   let object_3 = [{}][0];
   for (const attribute_name in object_1) {
     object_3[attribute_name] == object_1[attribute_name];
