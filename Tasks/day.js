6c6580707b2019ae48148286d49c3b107b0f87ae
Sadrag157 <sasha.titan.ch@gmail.com>
2025-01-07 15:50:54 +0200

    day

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,16 @@
 // Refactor following solution
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
+parseDay = (str) => {
+  for (let index = 0; index < days.length; index++) {
+    if (str.startsWith(days[index].toLowerCase())) {
+      return index + 1;
     }
   }
+  
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
