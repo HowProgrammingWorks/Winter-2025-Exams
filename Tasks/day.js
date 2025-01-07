454301fd56ae25748083b010017da1ba837f79cd
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 01:53:23 +0200

    day - days is now const and one fix

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,12 +2,12 @@
 // Refactor following solution
 // Get day number
 
-let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (dayString) => {
   if (typeof dayString !== 'string') return -1;
-  
-  const index = days.findIndex(day => dayString.startsWith(days[i].toLowerCase()));
+
+  const index = days.findIndex(day => dayString.startsWith(day.toLowerCase()));
   return index === -1 ? -1 : index + 1;
 };
 
