e9d4894f163f09077bf8ad6d2cb5637eed7c51bb
RickoBo <Vini.Marmone@gmail.com>
2025-01-07 03:50:32 +0200

    Step 6: prefer for..of instead for..in

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,15 +1,15 @@
 'use strict';
 // Merge two dictionaries
 
-// Step 5
-// Improve naming
+// Step 6
+// Prefer for..of instead for..in
 
 const mergeTwoObjects = (object1, object2) => {
   let mergedObject = {};
-  for (const key in object1) {
+  for (const key of Object.keys(object1)) {
     mergedObject[key] = object1[key];
   }
-  for (const key in object2) {
+  for (const key of Object.keys(object2)) {
     mergedObject[key] = object2[key];
   }
   return mergedObject;
