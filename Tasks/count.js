6976967d7e82027ee99d3df8795fdbb2cb9b0e40
ITrue-dev <den.glushkov.kv@gmail.com>
2025-01-08 00:43:51 +0200

    Add for of loop

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,18 @@
 // Refactor following solution
 // Sum all number values in dict
 
-'use strict'
+'use strict';
 
 const count = (obj) => {
   let sum = 0;
-  
+
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
-  });
+  }
   return sum;
 };
 
 module.exports = count;
+//Object.keys(obj).reduce((sum, value) => typeof obj[value] === 'number' ? sum + obj[value] : sum, 0);
