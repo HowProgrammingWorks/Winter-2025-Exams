a6ea47060f40f844b31fc751df46ae7452ddf27f
AleksD1203 <smiley3223337@gmail.com>
2025-01-08 17:06:39 +0200

    count

--- Tasks/count.js
+++ Tasks/count.js
@@ -6,14 +6,16 @@
 
 const count = (object) => {
   let sum = 0;
-
+  
   const keys = Object.keys(object);
+
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
     if (typeof object[key] === 'number') {
       sum += object[key];
     }
   }
+
   return sum;
 };
 
