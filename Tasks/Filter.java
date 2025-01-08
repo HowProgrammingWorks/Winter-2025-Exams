c80120b899a655c13ab0427fa3e1c5c8c42018ed
arcctg <91179695+arcctg@users.noreply.github.com>
2025-01-08 08:49:00 +0200

    Step 2

--- Tasks/Filter.java
+++ Tasks/Filter.java
@@ -1,23 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Step 1
+// Step 2
 //
-// Fix formatting
-// Improve naming
+// Remove senseless blocks of code
+// Don't mutate incoming parameters
+// Add empty line between semantic blocks
 
 public class Filter {
     public static <T> List<T> filter(List<T> list, String type) {
-        List<Integer> remove = new ArrayList<>();
+        List<T> filteredList = new ArrayList<>(list);
+
         for (T obj : list) {
-            int index = list.indexOf(obj);
-            if (!list.get(index).getClass().getSimpleName().equalsIgnoreCase(type)) {
-                remove.add(0, index);
+            if (obj.getClass().getSimpleName().equalsIgnoreCase(type)) {
+                filteredList.add(obj);
             }
         }
-        for (int index : remove) {
-            list.remove(index);
-        }
-        return list;
+
+        return filteredList;
     }
 }
