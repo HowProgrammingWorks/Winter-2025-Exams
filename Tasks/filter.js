5b9e38631c54c2a4f5d2908e2b89222912b839de
Khoanh Chan Kam Li <trancamly06@gmail.com>
2025-01-08 15:45:44 +0700

    Update filter.js: Improve function naming and add documentation
    // - Renamed function to follow camelCase convention
    // - Added JSDoc comments

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,4 +1,10 @@
-const Filter = (array, targetType) => {
+/**
+ * Filters array elements by their type
+ * @param {Array} array - The input array to filter
+ * @param {string} targetType - The type to filter by
+ * @returns {Array} The filtered array
+ */
+const filterByType = (array, targetType) => {
     let removeIndices = [];
     for (const element of array) {
         let currentIndex = array.indexOf(element);
@@ -9,4 +15,4 @@ const Filter = (array, targetType) => {
     for (const index of removeIndices) array.splice(index, 1);
     return array;
 };
-module.exports = Filter;
+module.exports = filterByType;
