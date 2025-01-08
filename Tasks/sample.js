6e7e7d52844a19cb2ec3c54009cd984451f39c4b
inryceu <pavel.maluev@gmail.com>
2025-01-08 17:23:18 +0200

    added strict, naming, avoided data mutability

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,9 +1,10 @@
 // Refactor following solution
 // Get one random element from an array
+"use strict";
 
-const SAMPLE = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
+const sample = (arr) => {
+  let element = arr[Math.floor(Math.random() * arr.length)];
+  return element;
 };
 
-module.exports = SAMPLE;
+module.exports = sample;
