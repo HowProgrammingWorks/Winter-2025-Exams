cad6ed5de1b342d6f41fea7667251fd484548f6b
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 02:30:00 +0200

    password - declaration

--- Tasks/password.js
+++ Tasks/password.js
@@ -2,11 +2,11 @@
 // Refactor following solution
 // Generate random password
 
-let generatePassword = (alphabet, length) => {
+const generatePassword = (alphabet, length) => {
   const max = alphabet.length;
-  key = '';
+  let key = '';
   for (let i = 0; i < length; i++) {
-    index = Math.floor(Math.random() * max);
+    const index = Math.floor(Math.random() * max);
     key = key + alphabet[index];
   }
   return key;
