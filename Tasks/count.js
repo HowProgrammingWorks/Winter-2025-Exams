c4bc28f9fef0f10e42ad5e33e362e584e1a21d8c
Mykola Semenenko <mikolasemenenko5@gmail.com>
2025-01-08 10:18:12 +0200

    Add empty lines between blocks

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,15 +1,15 @@
 // Refactor following solution
 // Sum all number values in dict
 
-//Step 5
-//Move expression to block for better readability
-//Use Object.values instead of Object.keys
+//Step 6
+//Add empty lines between blocks
 
 'use strict';
 
 const count = (obj) => {
   let sum = 0;
   const values = Object.values(obj);
+
   for (const value of values) {
     if (typeof value === 'number') {
       sum += value;
