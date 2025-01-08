7a4a807fbec5747e15aa75331fc419d3a265e44a
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2025-01-08 02:12:54 +0200

    Step 6: formatting

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,19 +1,20 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 5:
-// Do not mutate incoming arrays
-// Declare minLength
+// Step 6:
+// Formatting
 
 "use strict";
 
 const zip = (arr1 = [], arr2 = []) => {
-  const result = [];
-  const minLength = Math.min(arr1.length, arr2.length);
+  const result = [],
+    minLength = Math.min(arr1.length, arr2.length);
+
   for (let i = 0; i < minLength; i++) {
-    const cell = [arr1[i], arr2[i]];
+    const cell = [ arr1[i], arr2[i] ];
     result.push(cell);
   }
+
   return result;
 };
 
