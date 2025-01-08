b49718e7e7bc68adaed05eb20b1b9f33a8ec6b3d
Ann0123-cyber <anna.musiiovska@gmail.com>
2025-01-08 02:21:33 +0200

    improved formating

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,14 +2,12 @@
 // Get day number
 
 'use strict';
-let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDay = (nameOfDay) => {
-  let i;
-  for (i = 0; i < days.length; i++) {
-    if (nameOfDay.startsWith(days[i].toLowerCase())) {
-      return i + 1;
-    }
+  
+  for (let i = 0; i < DAYS.length; i++) {
+    if (nameOfDay.startsWith(DAYS[i].toLowerCase())) return i + 1;
   }
   return -1;
 };
