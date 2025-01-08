1df30d74d3b695fa1769cf62333a1a3a73790ad6
olexandrakk <aleksandra200689@gmail.com>
2025-01-08 19:59:29 +0200

    avoid mutating input parameters

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -5,16 +5,15 @@
 
 const distinct = (data) => {
   const uniqueVAlues = new Set();
-  let index = 0;
+  const result = [];
+
   data.forEach((item) => {
-    if (uniqueVAlues.has(item)) {
-      delete data[index];
-    } else {
+    if (!uniqueVAlues.has(item)) {
       uniqueVAlues.add(item);
+      result.push(item);
     }
-    index++;
   });
-  return data.filter((x) => typeof x === 'number');
+  return result.filter((x) => typeof x === 'number');
 };
 
 module.exports = distinct;
