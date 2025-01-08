badd36b5d6ee1698a64e4a001daa335ccf4c1e6a
niita4880 <niknikitich07@gmail.com>
2025-01-08 20:16:50 +0200

    task_duplicate_update

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,9 +2,10 @@
 // Return an array without duplicates
 
 duplicate = (value, N) => {
-  if (N <= 0) return [];
-  else {
-    res = [];
+  if (N <= 0) {
+    return [];
+  } else {
+    let res = [];
     for (let i = 0; i < N; i++) {
       res[i] = value;
     }
