71368726865879460ba8134b1e946ae63030baea
Ann0123-cyber <anna.musiiovska@gmail.com>
2025-01-08 02:17:13 +0200

    added strict mode, renamed variables using camelCase

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,17 @@
 // Refactor following solution
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+'use strict';
+let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
+const parseDay = (nameOfDay) => {
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < days.length; i++) {
+    if (nameOfDay.startsWith(days[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
