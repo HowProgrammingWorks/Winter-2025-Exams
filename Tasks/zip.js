6987c0e490cabb491d128e8c14b71b170df5afed
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2025-01-08 02:04:46 +0200

    Step 1: use "use strict", use const and let, prefer using arrow functions

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,11 +1,18 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-const zip = function (a = [], b = []) {
+// Step 1:
+// Use "use strict"
+// Use const and let
+// Prefer using arrow functions
+
+"use strict";
+
+const zip = (a = [], b = []) => {
   let i = 0;
-  j = 0;
-  for (x of b) {
-    CELL = [a[i++], x];
+  let j = 0;
+  for (const x of b) {
+    const CELL = [a[i++], x];
     if (i < j) {
       delete a[i++];
     } else {
