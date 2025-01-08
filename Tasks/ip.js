5474336c8ad57a16174a2f1bdaac5688805268b5
yarinapoian <poianyarina@gmail.com>
2025-01-08 02:49:10 +0100

    ip.js: improved readability

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -1,19 +1,23 @@
 // Refactor following solution
 // Split string by the first occurrence of separator
+'use strict';
 
 const parseIp = (ipString) => {
-  numbers = [];
-  if (ipString === '') return;
-  else {
-    parts = ipString.split('.');
-    if (parts.length != 4) return;
-    j = 0;
-    for (const part of parts) {
-      numbers[j] = parseInt(part);
-      if (isNaN(numbers[j])) return;
-      j++;
-    }
+  const parts = ipString.split('.');
+
+  if (!ipString || parts.length !== 4) {
+    return undefined;
   }
+
+  const numbers = [];
+  for (const part of parts) {
+    const num = parseInt(part);
+    if (isNaN(num) || num < 0 || num > 255) {
+      return undefined;
+    }
+    numbers.push(num);
+  }
+
   return numbers;
 };
 
