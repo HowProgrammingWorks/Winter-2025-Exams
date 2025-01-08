7858c729f18bd03acc251d4e57e7a305c72d289f
niita4880 <niknikitich07@gmail.com>
2025-01-08 19:52:51 +0200

    optimization

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,17 +2,9 @@
 // Sum all number values in dict
 
 const count = (obj) => {
-  let sum = 0;
-  const keys = Object.keys(obj);
-
-  keys.forEach((key) => {
-    const value = obj[key];
-    if (typeof value === 'number') {
-      sum += value;
-    }
-  });
-
-  return sum;
+  return Object.values(obj).reduce((sum, value) => {
+    return typeof value === 'number' ? sum + value : sum;
+  }, 0);
 };
 
 
