a5dcecb9ca66886333043048c95903a01c030c3d
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 02:31:53 +0200

    password - updated naming and simplifying

--- Tasks/password.js
+++ Tasks/password.js
@@ -4,12 +4,12 @@
 
 const generatePassword = (alphabet, length) => {
   const max = alphabet.length;
-  let key = '';
+  let password = '';
   for (let i = 0; i < length; i++) {
-    const index = Math.floor(Math.random() * max);
-    key = key + alphabet[index];
+    const randomIndex = Math.floor(Math.random() * max);
+    password += alphabet[randomIndex];
   }
-  return key;
+  return password;
 };
 
 module.exports = generatePassword;
