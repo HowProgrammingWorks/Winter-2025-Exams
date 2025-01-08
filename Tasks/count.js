e32000ffab234f803220d7c47db9a4bb16f0054f
SanQ1 <88672190+SanQ1@users.noreply.github.com>
2025-01-08 03:52:19 +0200

    count.js - move variable 'key' into cycle brackets; correct style

--- Tasks/count.js
+++ Tasks/count.js
@@ -2,12 +2,12 @@
 
 const count = (obj) => {
   let sum = 0;
-  
-  const keys = Object.keys(obj);
-  for(const key of keys)
+
+  for(const key of Object.keys(obj))
   {
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
+    if (typeof value === 'number')
+      sum += value;
   }
   
   return sum;
