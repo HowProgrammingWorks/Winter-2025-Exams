1fc154ff16db1b8c49cd4d77d7abda2d3ad744e8
niita4880 <niknikitich07@gmail.com>
2025-01-08 19:31:54 +0200

    new code_final

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -5,12 +5,17 @@ const compare = (obj1, obj2) => {
   const firstObjKeys = Object.keys(obj1);
   const secondObjKeys = Object.keys(obj2);
   let result = true
-  
-  if (firstKeys.length !== secondKeys.length || !firstKeys.every((key, index) => key === secondKeys[index])) {
-    return false;
+
+   if (firstObjKeys.join('-') !== secondObjKeys.join('-')) return false;
+  for (const key of firstObjKeys) {
+    if (obj1[key] === obj2[key]) result = result && true
+    else {
+      result = result && false
+    }
   }
 
-  return firstKeys.every(key => firstValues[key] === secondValues[key]);
+
+  return result;
 };
 
 module.exports = compare;
