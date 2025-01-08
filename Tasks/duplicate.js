dd084fff34b61c7e6fb22a780f87dc3af91ba2f1
niita4880 <niknikitich07@gmail.com>
2025-01-08 20:19:59 +0200

    task_duplicate_optimization

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -2,15 +2,8 @@
 // Return an array without duplicates
 
 duplicate = (value, N) => {
-  if (N <= 0) {
-    return [];
-  } else {
-    let res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
-  }
+  if (N <= 0) return [];
+  return Array(N).fill(value);
 };
 
 module.exports = duplicate;
