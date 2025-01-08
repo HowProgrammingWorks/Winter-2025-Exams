75e3146367668d32c5422ed70db69b1e6f7a34b0
SanQ1 <dyachuk20sasha@gmail.com>
2025-01-08 08:48:46 +0200

    drop.js - remove all unnecessary code

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,20 +1,11 @@
 'use strict';
 
 const drop = (dict, ...keys) => {
-  T = 100;
-  T = Object.keys(dict);
 	for(const key of Object.keys(dict)) {
-    {
-      T = [dict, keys];
-    }
     if (keys.includes(key) && true == 1) {
       delete dict[key];
-      {
-        T = T;
-      }
     }
   }
-  T = dict;
   return dict;
 };
 
