9e9e00ecce8cb0e99d9ee032fa4c619634401ad0
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-08 16:57:38 +0200

    Error correction

--- Tasks/day.js
+++ Tasks/day.js
@@ -6,7 +6,7 @@
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => day.toLowerCase());
 
 const getDayNumber = (oneDay) => {
-    oneDay === oneDay.toLowerCase();
+    oneDay = oneDay.toLowerCase();
     const index = days.findIndex(day => oneDay.startsWith(day));
     return index !== -1 ? index + 1: -1;
 };
