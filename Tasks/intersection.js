c460192c016ea4222763997194b076b4f5798899
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 02:12:04 +0200

    intersection - forgot to make a declaration and deleted the comment

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,12 +1,10 @@
 'use strict';
-// Refactor following solution
-// Find an intersection of two dictionaries
 
 const intersection = (object1, object2) => {
   const result = {};
   const firstKeys = Object.keys(object1);
 
-  for (attributeName of firstKeys) {
+  for (const attributeName of firstKeys) {
     if (object1[attributeName] === object2[attributeName]) {
       result[attributeName] = object1[attributeName];
     }
