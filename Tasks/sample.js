aab5d15e163a09590b74e097f444e11296d995df
Mykola Semenenko <mikolasemenenko5@gmail.com>
2025-01-08 11:16:24 +0200

    Add 'use strict';

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,9 +1,6 @@
 // Refactor following solution
 // Get one random element from an array
 
-const SAMPLE = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
-};
+const SAMPLE = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 module.exports = SAMPLE;
