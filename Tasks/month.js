69bd5b8c19e9f47ce27a74a0d930a321611b559b
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 02:17:15 +0200

    intersection - declaration

--- Tasks/month.js
+++ Tasks/month.js
@@ -5,8 +5,8 @@
 const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
           'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
-month = (s) => {
-  length = months.length;
+const month = (s) => {
+  const length = months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(months[i])) return i + 1;
   }
