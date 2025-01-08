b2eafed5a24417ecd9f495e5ca335541cea17db6
inryceu <pavel.maluev@gmail.com>
2025-01-08 18:25:09 +0200

    added strict, const

--- Tasks/size.js
+++ Tasks/size.js
@@ -1,7 +1,8 @@
 // Refactor following solution
 // Convert number to file size in bytes, kb, mb, and gb
+"use strict";
 
-let size = (size) => {
+const size = (size) => {
   if (size === 0) return '0 byte';
   else {
     const exp = Math.floor(Math.log(size) / Math.log(1000));
