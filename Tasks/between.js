fbd221ff031cdc27ab285a4e055d213cc3db3b24
Viktor Montazer <montazer.viktor@lll.kpi.ua>
2025-01-08 18:33:50 +0200

    beetween.js has been refactored

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,22 +1,19 @@
-// Refactor following solution
-// Extract substring between prefix and suffix
+'use strict'
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
-  if (i === -1) return '';
-  else {
-    k = i + p.length;
-    str = str.substring(k);
-    if (s) {
-      i = str.indexOf(s);
-      if (i === -1) {
-        return '';
-      } else {
-        str = str.substring(0, i);
-      }
-    }
+
+const getValueBeetween = (str, startValue, endValue) => {
+  const startIndex = str.indexOf(startValue);
+  if (startIndex === -1) return '';
+
+  const start = startIndex + startValue.length
+
+  if(endValue) {
+    const end = str.indexOf(endValue)
+    if(end === -1) return ''
+    return str.substring(start, end)
   }
-  return str;
+
+  return str.substring(start)
 };
 
-module.exports = getvaluebetween;
+module.exports = getValueBeetween;
