51087b59257a03ff3e3d97c2a58d2a49cf7ba50b
arcctg <91179695+arcctg@users.noreply.github.com>
2025-01-08 09:30:00 +0200

    Step 2

--- Tasks/Ip.java
+++ Tasks/Ip.java
@@ -1,36 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Step 1
+// Step 2
 //
-// Fix formatting
-// Add empty line between semantic blocks
-// Add constant for IP size
-// Rename variables to be more descriptive
-// Replace equals("") with isEmpty()
+// Remove unnecessary if-else block
+// Replace try-catch block with regex check
 
 public class Ip {
     private static final int IP_SIZE = 4;
 
     public static List<Integer> parseIp(String ip) {
+        if (ip.isEmpty()) return null;
+
+        String[] segments = ip.split("\\.");
+        if (segments.length != IP_SIZE) return null;
+
         List<Integer> ipSegments = new ArrayList<>();
-
-        if (ip.isEmpty()) {
-            return null;
-        } else {
-            String[] segments = ip.split("\\.");
-
-            if (segments.length != IP_SIZE) return null;
-
-            int index = 0;
-            for (String segment : segments) {
-                try {
-                    ipSegments.add(index, Integer.parseInt(segment));
-                } catch (NumberFormatException e) {
-                    return null;
-                }
-
-                index++;
+        for (String segment : segments) {
+            if (segment.matches("\\d+")) {
+                ipSegments.add(Integer.parseInt(segment));
+            } else {
+                return null;
             }
         }
 
