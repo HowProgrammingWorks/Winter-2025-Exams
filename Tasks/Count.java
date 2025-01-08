bd18e632852fa4b5246b8ad09c32dc83f9e292f5
arcctg <91179695+arcctg@users.noreply.github.com>
2025-01-08 09:13:00 +0200

    Step 4

--- Tasks/Count.java
+++ Tasks/Count.java
@@ -1,18 +1,18 @@
 import java.util.Map;
 
-// Step 3
+// Step 4
 //
-// Replace Collection.forEach() with for-each loop
-// Replace atomic sum with int sum
-// Iterate over the "entrySet" instead of the "keySet"
-
+// Format code
+// Add empty line between semantic blocks
 
 public class Count {
     public static <K, V> int count(Map<K, V> map) {
         int sum = 0;
+
         for (Map.Entry<K, V> set : map.entrySet()) {
             if (set.getValue() instanceof Number number) sum += number.intValue();
         }
+
         return sum;
     }
 }
