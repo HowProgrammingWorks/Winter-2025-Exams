2a3de2121d2affb77e8e22846e943d0d7fcd61da
niita4880 <niknikitich07@gmail.com>
2025-01-08 20:10:13 +0200

    task_drop_update_of_flexibility

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,13 +1,11 @@
 // Refactor following solution
 // Delete listed keys from dictionary
 
-DroP = (D, ...X) => {
-  Object.keys(D).forEach((key) => {
-    if (X.includes(key)) {
-      delete D[key];
-    }
-  });
+DroP = (D = {}, ...X) => {
+  if (typeof D !== "object" || D === null) return {};
+  Object.keys(D).filter((key) => X.includes(key)).forEach((key) => delete D[key]);
   return D;
 };
+
 
 module.exports = DroP;
