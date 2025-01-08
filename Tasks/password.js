e3ca14ec2680f4e527c6bdd146fb4df4e3785cbb
Khoanh Chan Kam Li <trancamly06@gmail.com>
2025-01-08 15:36:16 +0700

    Update password.js: Improve parameter naming
    Improve parameter naming
    - Rename alphabet to allowedChars for clarity

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,25 +1,25 @@
 /**
  * Generates a random password of specified length using provided character set
- * @param {string} alphabet - String of characters to use for password generation
+ * @param {string} allowedChars - String of characters to use for password generation
  * @param {number} length - Desired length of the password
  * @returns {string} Generated password
- * @throws {Error} If length is negative or alphabet is empty
+ * @throws {Error} If length is negative or allowedChars is empty
  */
-const generatePassword = (alphabet, length) => {
+const generatePassword = (allowedChars, length) => {
     if (length < 0) {
         throw new Error('Password length must be non-negative');
     }
     
-    if (!alphabet || alphabet.length === 0) {
+    if (!allowedChars || allowedChars.length === 0) {
         throw new Error('Character set cannot be empty');
     }
 
-    const maxLength = alphabet.length;
+    const maxLength = allowedChars.length;
     const passwordChars = [];
     
     for (let i = 0; i < length; i++) {
         const index = Math.floor(Math.random() * maxLength);
-        passwordChars.push(alphabet[index]);
+        passwordChars.push(allowedChars[index]);
     }
     return passwordChars.join('');
 };
