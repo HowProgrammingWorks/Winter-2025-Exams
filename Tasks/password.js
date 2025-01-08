2ad34b03b600639560fa93bfbc6e63dab9abb7aa
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 02:34:41 +0200

    password - more readability and deleted the comment

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,10 +1,9 @@
 'use strict';
-// Refactor following solution
-// Generate random password
 
 const generatePassword = (alphabet, length) => {
   const max = alphabet.length;
   let password = [];
+
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * max);
     password.push(alphabet[randomIndex]);
