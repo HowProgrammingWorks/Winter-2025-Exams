be43b5217b9c2f915e6cc52188c8c877bb98b32a
saloma6661 <solomkamisha516@gmail.com>
2025-01-08 20:06:46 +0200

    Refactored

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,18 @@
+'use strict'
 // Refactor following solution
 // Get day number
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const getDayNum = (str) => {
+  if (typeof str !== `string`) return -1;
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
-      return i + 1;
-    }
+  for (let num = 0; num < DAYS.length; num++) {
+    let isDay = str.startsWith(DAYS[num].toLowerCase());
+
+    if (isDay) return num + 1;
   }
+
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = getDayNum;
