803ea68890865d8bf8245517292af4ac93772662
hardbass2525 <maksimcbr2000@gmail.com>
2025-01-08 02:22:24 +0200

    intersection - input validation

--- Tasks/month.js
+++ Tasks/month.js
@@ -6,6 +6,8 @@ const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
                 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
 const month = (s) => {
+  if (typeof s !== 'string') return -1;
+  
   const index = month.findIndex(month => s.toLowerCase().startsWith(month));
   return index === -1 ? -1 : index + 1;
 };
