f12891a54f4a5745e1f60493eb45290190d59d98
arcctg <91179695+arcctg@users.noreply.github.com>
2025-01-08 09:16:00 +0200

    Step 5

--- Tasks/Count.java
+++ Tasks/Count.java
@@ -1,18 +1,15 @@
 import java.util.Map;
 
-// Step 4
+// Step 5
 //
-// Format code
-// Add empty line between semantic blocks
+// Add Java streams
+// Add lambda expression
 
 public class Count {
     public static <K, V> int count(Map<K, V> map) {
-        int sum = 0;
-
-        for (Map.Entry<K, V> set : map.entrySet()) {
-            if (set.getValue() instanceof Number number) sum += number.intValue();
-        }
-
-        return sum;
+        return map.values().stream()
+                .filter(Number.class::isInstance)
+                .mapToInt(value -> ((Number) value).intValue())
+                .sum();
     }
 }
