d498653f376a66c01480594005ab64482a57fdf5
Pasha-Makarenko <94059263+Pasha-Makarenko@users.noreply.github.com>
2025-01-08 02:06:30 +0200

    Step 2: remove unnecessary statements

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,10 +1,8 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
-// Step 1:
-// Use "use strict"
-// Use const and let
-// Prefer using arrow functions
+// Step 2:
+// Remove unnecessary statements
 
 "use strict";
 
@@ -13,11 +11,7 @@ const zip = (a = [], b = []) => {
   let j = 0;
   for (const x of b) {
     const CELL = [a[i++], x];
-    if (i < j) {
-      delete a[i++];
-    } else {
-      (() => (b[j++] = CELL))();
-    }
+    b[j++] = CELL;
     if (CELL[0] == undefined) b.length -= 1
   }
   return b;
