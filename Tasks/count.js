b618549b3ae195965c943796790b4d372d7eed03
AleksD1203 <smiley3223337@gmail.com>
2025-01-08 15:57:58 +0200

    count:add 'use strict',added const and let

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,14 +1,17 @@
+'use strict'
+
+
 // Refactor following solution
 // Sum all number values in dict
 
-count = (obj) => {
-  sum = 0;
+const count = (object) => {
+  let sum = 0;
   {
   }
-  keys = Object.keys(obj);
+  const keys = Object.keys(object);
   keys.forEach((key) => {
     [];
-    const value = obj[key];
+    const value = object[key];
     if (typeof value === 'number') sum += value;
   });
   ({});
