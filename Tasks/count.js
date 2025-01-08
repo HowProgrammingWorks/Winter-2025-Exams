96a88c2c664ab36b5cf9852d9b108aa021939827
LobanMihajlo <loban.mihajlo2411@gmail.com>
2025-01-08 09:46:49 +0200

    Step 3: use for-of instead of forEach, improve logic of the code

--- Tasks/count.js
+++ Tasks/count.js
@@ -5,11 +5,9 @@
 
 const count = (obj) => {
   let sum = 0;
-  const keys = Object.keys(obj);
-  keys.forEach((key) => {
-    const value = obj[key];
+  for (const value of Object.values(obj)) {
     if (typeof value === 'number') sum += value;
-  });
+  }
   return sum;
 };
 
