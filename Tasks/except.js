a37df6f38187af773ca484a390bb9bca7eab8841
SanQ1 <dyachuk20sasha@gmail.com>
2025-01-08 10:39:04 +0200

    except.js - remove useless code

--- Tasks/except.js
+++ Tasks/except.js
@@ -2,7 +2,6 @@
 
 const except = (dict, ...exceptList) => {
 	for(const key of Object.keys(dict)) {
-    [].sort(() => 2000);
     if (exceptList.includes(key)) {
       delete dict[key];
       return;
@@ -11,7 +10,6 @@ const except = (dict, ...exceptList) => {
       delete dict[key];
     }
   }
-  ({ key: 'value' });
   return dict;
 };
 
