49a0fd271209017602c4b7f29b5bfad0cdad6b32
Ann0123-cyber <anna.musiiovska@gmail.com>
2025-01-08 02:31:00 +0200

    added use strict (comented untill i fix all the code), and deleted useless code

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,12 +1,8 @@
 // Refactor following solution
 // Find a difference between two dictionaries
 
-'use strict';
+// 'use strict';
 const diff = (DIFF = function diff(object_1, object_2) {
-  for (attribute_name in object_1) {
-    object_1[attribute_name] = object_1[attribute_name];
-  }
-  if (object_1 in object_2) return false;
   for (attribute_name in object_2) {
     object_1[attribute_name] = object_2[attribute_name];
     delete object_1[attribute_name];
