0fbbf09579dda52ce2f239d41bf4b9220c229d8f
yarinapoian <poianyarina@gmail.com>
2025-01-08 01:07:42 +0100

    improved refactored code to make it more readable

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -1,18 +1,19 @@
 // Refactor following solution
 // Compare two dictionaries
+'use strict';
+const compare = (firstValues, secondValues) => {
 
-let compare = (firstValues, secondValues) => {
   let firstKeys = Object.keys(firstValues);
   let secondKeys = Object.keys(secondValues);
-  if (firstKeys.join('-') !== secondKeys.join('-')) return false;
-  let e = true;
-  for (c of firstKeys) {
-    if (firstValues[c] === secondValues[c]) e = e && true;
-    else {
-      e = e && false;
+  if (firstKeys.length !== secondKeys.length) {
+    return false;
+  }
+  for (const key of firstKeys) {
+    if (secondKeys.indexOf(key) === -1 || firstValues[key] !== secondValues[key]) {
+      return false;
     }
   }
-  return e;
+  return true;
 };
 
 module.exports = compare;
