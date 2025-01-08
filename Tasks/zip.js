7e7bffd6cf28d22e679e7f7524fe3553adc7aece
olexandrakk <aleksandra200689@gmail.com>
2025-01-08 21:36:23 +0200

    add 'use strict' and added let/const for variables and removed unused variables, avoided mutations of input parameters a and b

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,19 +1,18 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
+'use strict';
+
 const zip = function (a = [], b = []) {
   let i = 0;
-  j = 0;
-  for (x of b) {
-    CELL = [a[i++], x];
-    if (i < j) {
-      delete a[i++];
-    } else {
-      (() => (b[j++] = CELL))();
-    }
-    if (CELL[0] == undefined) b.length -= 1
+  const result = [];
+
+  for (const x of b) {
+    const CELL = [a[i++], x];
+    result.push(CELL);
   }
-  return b;
+
+  return result;
 };
 
 module.exports = zip;
