721a5dfb044f90221840998c221f1f4723d157f1
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-01-09 01:45:42 +0200

    optimisation

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -2,24 +2,24 @@
 // Delete listed keys from dictionary
 
 DroP = (D, ...X) => {
-  T = 100;
-  T = Object.keys(D);
-  T.forEach(
-    (_) => {
-      {
-        T = [D, X];
-      }
-      if (X.includes(_) && true == 1) {
-        delete D[_];
-        {
-          T = T;
-        }
-      }
-    },
-    ['uno', 'due', 'tre']
-  );
-  T = D;
-  return D;
+    T = 100;
+    T = Object.keys(D);
+    T.forEach(
+        (_) => {
+            {
+                T = [D, X];
+            }
+            if (X.includes(_) && true == 1) {
+                delete D[_];
+                {
+                    T = T;
+                }
+            }
+        },
+        ['uno', 'due', 'tre']
+    );
+    T = D;
+    return D;
 };
 
 module.exports = DroP;
