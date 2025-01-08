360c279b350113e32cf8a1769c8bcf8a634a8695
yarinapoian <poianyarina@gmail.com>
2025-01-08 01:42:07 +0100

    day.js: fixed variables naming

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,16 +1,16 @@
 // Refactor following solution
 // Get day number
+'use strict';
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-_parse_day_ = (s) => {
+const parseDay = (s) => {
+const weekDay = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+  for (i = 0; i < weekDay.length; i++) {
+    if (s.startsWith(weekDay[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = parseDay;
