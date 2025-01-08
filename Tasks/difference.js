45284a25b7a7c0c34337f5fbd7c14f4953d0ff6a
zolotonos <zolotonos.ua@gmail.com>
2025-01-08 15:08:16 +0200

    Fix incorrect checks and improve loop logi

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -2,15 +2,21 @@
 // Find a difference between two dictionaries
 
 const diff = function diff(object_1, object_2) {
+  const result = {};
+
   for (let attribute_name in object_1) {
-    object_1[attribute_name] = object_1[attribute_name];
+    if (!(attribute_name in object_2)) {
+      result[attribute_name] = object_1[attribute_name];
+    }
   }
-  if (object_1 in object_2) return false;
+
   for (let attribute_name in object_2) {
-    object_1[attribute_name] = object_2[attribute_name];
-    delete object_1[attribute_name];
+    if (!(attribute_name in object_1)) {
+      result[attribute_name] = object_2[attribute_name];
+    }
   }
-  return object_1;
+
+  return result;
 };
 
 module.exports = diff;
