b3f6d28f4fe0fbd02b7adf16dd6e1831c0201254
Khoanh Chan Kam Li <trancamly06@gmail.com>
2025-01-08 15:45:17 +0700

    Update filter.js: Add proper variable declarations
    - Added const/let declarations
    - Fixed function declaration

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,13 +1,12 @@
-Filter = (array, targetType) => {
-    removeIndices = [];
-    for (element of array) {
-        currentIndex = array.indexOf(element);
+const Filter = (array, targetType) => {
+    let removeIndices = [];
+    for (const element of array) {
+        let currentIndex = array.indexOf(element);
         if (typeof array[currentIndex] !== targetType) {
             removeIndices.unshift(currentIndex);
         }
     }
-    for (index of removeIndices) array.splice(index, 1);
+    for (const index of removeIndices) array.splice(index, 1);
     return array;
 };
-
 module.exports = Filter;
