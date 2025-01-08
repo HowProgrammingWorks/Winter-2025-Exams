6e5889190b3e38db196d7a3baf5918bc3e6d7390
Khoanh Chan Kam Li <trancamly06@gmail.com>
2025-01-08 15:35:06 +0700

    Update password.js: add input validation
    Add input validation
    - Validate length parameter
    - Validate alphabet parameter
    - Add descriptive error messages

--- Tasks/password.js
+++ Tasks/password.js
@@ -3,8 +3,17 @@
  * @param {string} alphabet - String of characters to use for password generation
  * @param {number} length - Desired length of the password
  * @returns {string} Generated password
+ * @throws {Error} If length is negative or alphabet is empty
  */
 const generatePassword = (alphabet, length) => {
+    if (length < 0) {
+        throw new Error('Password length must be non-negative');
+    }
+    
+    if (!alphabet || alphabet.length === 0) {
+        throw new Error('Character set cannot be empty');
+    }
+
     const maxLength = alphabet.length;
     let password = '';
     
