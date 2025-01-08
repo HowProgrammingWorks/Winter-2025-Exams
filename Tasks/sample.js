34ee9944a70927b0797a744bff30316102c8eec2
Mykola Semenenko <mikolasemenenko5@gmail.com>
2025-01-08 11:20:29 +0200

    Rewrite function in one line

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,14 +1,11 @@
 // Refactor following solution
 // Get one random element from an array
 
-//Step 2
-//Rename variable for better understanding
+//Step 3
+//Rewrite the function in one line
 
 'use strict';
 
-const randomElement = (arr) => {
-  arr = arr[Math.floor(Math.random() * arr.length)];
-  return arr;
-};
+const randomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 module.exports = randomElement;
