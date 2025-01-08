1979078088cc31def1d273420c4cc62e3b7df7c9
SanQ1 <dyachuk20sasha@gmail.com>
2025-01-08 08:11:37 +0200

    replace.js - some style corrections

--- Tasks/replace.js
+++ Tasks/replace.js
@@ -4,19 +4,20 @@ const replace = (str, substr, newstr) => {
   if (substr === '')
     return str;
 
-  let temp = str;
   let res = '';
+  let temp = str;
 
   let index = temp.indexOf(substr);
   while(index !== -1) {
     const start = temp.substring(0, index);
     temp = temp.substring(index + substr.length, temp.length);
-    res += start + newstr;
+    res += (start + newstr);
 
-		index = temp.indexOf(substr);
+    index = temp.indexOf(substr);
   } 
+  res += temp;
 
-  return res + temp;
+  return res;
 };
 
 module.exports = replace;
