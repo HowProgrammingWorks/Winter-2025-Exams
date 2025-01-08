0e01e580c4aa2b960396c552cfa10730da829708
AleksD1203 <smiley3223337@gmail.com>
2025-01-08 19:12:08 +0200

    compare:rewrited

--- Tasks/compare.js
+++ Tasks/compare.js
@@ -4,13 +4,17 @@
 // Refactor following solution
 // Compare two dictionaries
 
-let compare = (first_values,second_values) => {
+let compare = (first_values, second_values) => {
   const keyA = Object.keys(first_values);
   const keyB = Object.keys(second_values);
+
   if (keyA.length !== keyB.length) return false;
+  
   for (let i=0; i < keyA.length; i++) {
     const key = keyA[i];
-    if (key !== keyB[i] || first_values[key] !== second_values[key]) return false;
+    if (key !== keyB[i] || first_values[key] !== second_values[key]) {
+    return false;
+    }
   }
 
   return true;
