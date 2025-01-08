999058f1304a6a4f81d7f477cf110c069efb56b1
AleksD1203 <smiley3223337@gmail.com>
2025-01-08 14:44:11 +0200

    Refactored between:added 'use strict' and did some changes

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,21 +1,23 @@
+'use strict'
+
+
 // Refactor following solution
 // Extract substring between prefix and suffix
 
 const getValueBetween = (string, prefix, suffix) => {
   const prefixindex = string.indexOf(prefix);
   if (prefixindex === -1) return '';
-
-   const beginning = prefixindex + prefix.length;
-   const stringAfterPrefix = string.substring(beginning);
+   
+    const beginning = prefixindex + prefix.length;
+    const substringAfterPrefix = string.substring(beginning);
 
     if (suffix) {
-    const suffixindex = stringAfterPrefix.indexOf(suffix);
+    const suffixindex = substringAfterPrefix.indexOf(suffix);
     if (suffixindex === -1) return '';
-     
-    suffixstring = stringAfterPrefix.substring(0, suffixindex);
-    }
+    return substringAfterPrefix.substring(0, suffixindex);
+  }
 
-      return suffixstring
-    };
+  return substringAfterPrefix
+};
 
 module.exports = getValueBetween;
