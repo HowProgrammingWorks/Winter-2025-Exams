39fe3d1d59d72c1b8b2ffd0f13f5465260dfb174
Mykola Semenenko <mikolasemenenko5@gmail.com>
2025-01-08 13:21:41 +0200

    Declare a new variable to prevent mutation

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,21 +1,22 @@
 // Refactor following solution
 // Find an intersection of two dictionaries
 
-//Step 3
-//Remove unneeded assigning and simplify the condition
+//Step 4
+//Declare a new variable to prevent mutation
 
 'use strict';
 
 const intersection = (object_1, object_2) => {
   const firstKeys = Object.keys(object_1);
+  const result = {};
 
   for (const attributeName of firstKeys) {
-    if (object_1[attributeName] !== object_2[attributeName]) {
-      delete object_1[attributeName];
+    if (object_1[attributeName] === object_2[attributeName]) {
+      result[attributeName] = object_1[attributeName];
     }
   }
 
-  return object_1;
+  return result;
 };
 
 module.exports = intersection;
