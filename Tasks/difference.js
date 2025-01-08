85b369260ed859b490f2d17915c1db22c045d32d
zolotonos <zolotonos.ua@gmail.com>
2025-01-08 15:11:16 +0200

    Add filtering for own properties using hasOwnProperty

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -5,13 +5,19 @@ const diff = function diff(object_1, object_2) {
   const result = {};
 
   for (let attribute_name in object_1) {
-    if (!(attribute_name in object_2)) {
+    if (
+      object_1.hasOwnProperty(attribute_name) &&
+      !(attribute_name in object_2)
+    ) {
       result[attribute_name] = object_1[attribute_name];
     }
   }
 
   for (let attribute_name in object_2) {
-    if (!(attribute_name in object_1)) {
+    if (
+      object_2.hasOwnProperty(attribute_name) &&
+      !(attribute_name in object_1)
+    ) {
       result[attribute_name] = object_2[attribute_name];
     }
   }
