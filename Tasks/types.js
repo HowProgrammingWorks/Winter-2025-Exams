66980a2ae376fccff4c90e200a3add88e25b47ff
Ilya Sizonenko <sizonenkoilya0@gmail.com>
2025-01-09 23:36:56 +0200

    problems with types.js comment

--- Tasks/types.js
+++ Tasks/types.js
@@ -3,12 +3,16 @@
 
 //Fourth step: rename variables, avoid using of the same name
 
+//From gitHub comment: "It is better to create empty collection {} and add keys just after detecting elenents of certain type"
+//Yes, it is logic, but there is a problem that tests just doesn't pass with that
+
 const types = (array) => {
   const typeCounters = {
     number: 0,
     string: 0,
     boolean: 0,
   };
+  
   for (const el of array) {
     const type = typeof el;
     typeCounters[type]++;
