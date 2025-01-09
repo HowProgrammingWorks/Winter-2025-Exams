c987fcfcf838f43c5568efeec0efb4cdacecc36f
Ilya Sizonenko <sizonenkoilya0@gmail.com>
2025-01-09 23:09:18 +0200

    fix filter function

--- Tasks/filter.js
+++ Tasks/filter.js
@@ -1,10 +1,10 @@
+`use strict`;
+
 // Refactor following solution
 // Filter array by type name
 
-//Fifth step: replace for..of loop with filter function 
+//Fifth step: replace for..of loop with filter function
 
-const filter = (array, type) => {
-  return array.filter(e => typeof e === type);
-};
+const filterByName = (array, type) => array.filter((e) => typeof e === type);
 
-module.exports = filter;
+module.exports = filterByName;
