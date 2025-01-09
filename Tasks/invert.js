f3b6c477ca62548d86f36c759c8d1a6d64a5ca15
MihajloVat <vatolkinmihajlo@gmail.com>
2025-01-09 20:21:39 +0200

    invert (from scratch,fixed version)
    early "tempo" declaration was a blunder, so I removed this.

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,11 +1,10 @@
 // Reverse an array, you can't use .reverse()
 
 const invert = (array) => {
-  let tempo = null;
   const last = array.length - 1;
   const half = Math.floor(last / 2);
   for (let i = 0; i <= half; i++) {
-    tempo = array[last - i];
+    let tempo = array[last - i];
     array[last - i] = array[i];
     array[i] = tempo;
   }
