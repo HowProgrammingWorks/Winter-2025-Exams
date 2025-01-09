17c8137fb9eb94ee0a0f426ca601579e32125bf3
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-09 22:56:27 +0200

    5. use "lenght" variable and declatation using keyword "new" to avoid resizing an array

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,13 +1,16 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-// Step 5. rewrite func using while-loop
+// Step 5. use "lenght" variable and declatation using keyword "new" to avoid resizing an array
 
 'use strict';
 
 const invert = (array) => {
-  const result = [];
-  while (array.length) result.push(array.pop());
+  const length = array.length;
+  const result = new Array(length);
+  for (let i = 0; i < length; i++) {
+    result[i] = array[length - 1 - i];
+  }
   return result;
 };
 
