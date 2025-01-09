5b672666351a6904bad205465b6161e997080cb9
LobanMihajlo <loban.mihajlo2411@gmail.com>
2025-01-09 22:19:22 +0200

    Step 5: fix naming; use || instead of ternary operator

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,11 +3,11 @@
 
 'use strict';
 
-const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (day) => {
-  const index = days.findIndex((d) => day.startsWith(d));
-  return index === -1 ? index : index + 1;
+  const index = DAYS.findIndex((d) => day.startsWith(d));
+  return index + 1 || -1;
 };
 
 module.exports = parseDay;
