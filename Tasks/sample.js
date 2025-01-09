85e6978e8c0c6feec8a756c618609659749a9286
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-09 23:14:51 +0200

    4. decomposed to random(min, max) and sample(array)

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,10 +1,12 @@
 // Refactor following solution
 // Get one random element from an array
 
-// Step 3. remove unnecessary intermediate variable
+// Step 4. decomposed to random(min, max) and sample(array) 
 
 'use strict';
 
-const sample = (array) => array[Math.floor(Math.random() * array.length)];
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const sample = (array) => array[random(0, array.length - 1)];
 
 module.exports = sample;
