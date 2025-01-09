32e8c47b4ef1a903a6c2805c9f38aa2eb3a1aac7
itssayzy <flicks2006@gmail.com>
2025-01-09 23:24:48 +0200

    add a const to func and max = min; made func more readable and understandable

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,13 +1,11 @@
 // Refactor following solution
 // Generate random integer value in given range
 
-Random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
+ const Random = (min, max = min) => {
+  if (max === min) {
+    min = 0;
   }
+    return min + Math.floor(Math.random() * (max - min + 1));
 };
 
 module.exports = Random;
