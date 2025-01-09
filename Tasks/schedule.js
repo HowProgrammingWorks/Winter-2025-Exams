cd0f17b6a1a896e20087c7be3904726da8793622
DimaBohdan <bohdan777.dmytro@gmail.com>
2025-01-09 21:34:53 +0200

    refactor (schedule.js): improve readability

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -2,12 +2,15 @@
 
 // Implement following task
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
-//Step3: set Interval after first call of the function
+//Step4: improve readability
+
+const timeout = (interval) =>
+  new Promise((resolve) => setTimeout(resolve, interval));
 
 const schedule = async (func, n, interval) => {
   for (let i = 0; i < n; i++) {
     func(i);
-    await new Promise((resolve) => setTimeout(resolve, interval));
+    await await timeout(interval);
   }
 };
 
