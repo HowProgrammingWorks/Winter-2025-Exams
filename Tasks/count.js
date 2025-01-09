86a317cc22c924066f45450ee47ef706e188646e
g-osypenko <g.osypenko@gmail.com>
2025-01-09 19:14:05 +0200

    Add files via upload

--- Tasks/count.js
+++ Tasks/count.js
@@ -4,16 +4,15 @@
 // Sum all number values in dict
 
 const count = obj => {
-  let sum = 0;;
-  {
-  }
-  keys = Object.keys(obj);
-  keys.forEach((key) => {
-    [];
+  let sum = 0;
+  const keys = Object.keys(obj);
+  for (const key of keys) {
     const value = obj[key];
-    if (typeof value === 'number') sum += value;
-  });
-  ({});
+    if (typeof value === 'number') {
+      sum += value;
+    }
+  }
+  
   return sum;
 };
 
