2d8212510dccf7b8f4d223fb526d7f2c0ad46747
g-osypenko <g.osypenko@gmail.com>
2025-01-09 19:19:29 +0200

    Add files via upload

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,14 @@
+`use strict`
+
+
 // Refactor following solution
 // Get day number
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];;
 
 _parse_day_ = (s) => {
   let i;
-  for (i = 0; i < D.length; i++) {
+  for (i = 0; i < DAYS.length; i++) {
     if (s.startsWith(D[i].toLowerCase())) {
       return i + 1;
     }
