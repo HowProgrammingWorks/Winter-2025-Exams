89133e26c38faa30219d0a14b7cc98eed4ffdf0f
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-09 23:19:48 +0200

    5. made code human-readable in general using intermediate identifiers

--- Tasks/section.js
+++ Tasks/section.js
@@ -1,16 +1,19 @@
 // Refactor following solution
 // Split string into array by the first occurrence of separator
 
-// Step 4. refix naming and rewrite into functional style with ternary operator
+// Step 5. made code human-readable in general using intermediate identifiers
 
 'use strict';
 
-const sectionOfString = (str, separator) =>
-  separator && str.includes(separator)
-    ? [
-        str.slice(0, str.indexOf(separator)),
-        str.slice(str.indexOf(separator) + separator.length),
-      ]
-    : [str, ''];
+const sectionOfString = (str, separator) => {
+  if (separator && str.includes(separator)) {
+    const separatorIndex = str.indexOf(separator);
+    const firstPart = str.slice(0, separatorIndex); 
+    const secondPart = str.slice(separatorIndex + separator.length);
+    return [firstPart, secondPart];
+  }
+  return [str, ''];
+}
+  
 
 module.exports = sectionOfString;
