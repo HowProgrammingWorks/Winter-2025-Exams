4c1594f0a4812197de234a028a163ded9409113e
arcctg <91179695+arcctg@users.noreply.github.com>
2025-01-09 10:58:32 +0200

    Step 4

--- Tasks/Words.java
+++ Tasks/Words.java
@@ -1,16 +1,13 @@
-// Step 3
+// Step 4
 //
-// Simplify code by using regex
-// Add empty lines for better readability
+// Combine the if statement and return statement using
+// a ternary operator for conciseness
+// Add empty line for better readability
 
 public class Words {
     public static int countWords(String input) {
         String trimmedInput = input.trim();
 
-        if (trimmedInput.isEmpty()) {
-            return 0;
-        }
-
-        return trimmedInput.split("\\s+").length;
+        return trimmedInput.isEmpty() ? 0 : trimmedInput.split("\\s+").length;
     }
 }
