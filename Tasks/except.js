742cfe168347eafc61fade34b333a2a09195ef8e
Kalienchenko Mykhailo <blvckwhite007@gmail.com>
2025-01-09 22:06:43 +0200

    7. replace destructuring in filter method for better optimization

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,13 +1,13 @@
 // Refactor following solution
 // Copy all values from dict except listed
 
-// Step 6. replace forEach-loop with .fromEntries to optimize code style
+// Step 7. using suggestions replace destructuring in filter method for better optimization
 
 "use strict";
 
 const except = (obj, ...exceptions) => {
   return Object.fromEntries(
-    Object.entries(obj).filter(([key]) => !exceptions.includes(key)),
+    Object.entries(obj).filter((entry) => !exceptions.includes(entry[0])),
   );
 };
 
