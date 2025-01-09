984b655b1add4bb3d81352ffb9e12f12dd96550a
itssayzy <flicks2006@gmail.com>
2025-01-09 23:00:04 +0200

    return difference

--- Tasks/difference.js
+++ Tasks/difference.js
@@ -1,9 +1,14 @@
 const diff = (object_1, object_2) => {
+  const differences = {};
+
   for (const key in object_1) {
     if (!(key in object_2)) {
-
+      differences[key] = object_1[key];
     }
   }
+
+
+  return differences;
 };
 
 module.exports = diff;
