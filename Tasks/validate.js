c090185b07f317e92c601aa4ce18162c609cfd0f
Oleksandr Cherepov <sashacherepov55@gmail.com>
2025-01-10 12:36:37 +0200

    Refactor: change function naming and extract hardcoded values

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -2,15 +2,19 @@
 
 'use strict';
 
-const isValidate = (name) => {
-  if (!name.includes(' ')) return false;
+const validateName = (name) => {
+  const MIN_CODE = 97;
+  const MAX_CODE = 122;
+  const SPACE_CHAR = ' ';
+
+  if (!name.includes(SPACE_CHAR)) return false;
   for (const char of name) {
     const code = char.toLowerCase().charCodeAt(0);
-    if ((code < 97 || code > 122) && char !== ' ') {
+    if ((code < MIN_CODE || code > MAX_CODE) && char !== SPACE_CHAR) {
       return false;
     }
   }
   return true;
 };
 
-module.exports = isValidate;
+module.exports = validateName;
