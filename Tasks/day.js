3f240ece723a94dab61a4e1659198ce8620c3477
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-10 11:32:59 +0200

    Change return as you recommended

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,14 +1,16 @@
 // Refactor following solution
 // Get day number
 
-'use strict'
+'use strict';
 
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(day => day.toLowerCase());
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day) =>
+  day.toLowerCase(),
+);
 
 const getDayNumber = (oneDay) => {
-    oneDay = oneDay.toLowerCase();
-    const index = days.findIndex(day => oneDay.startsWith(day));
-    return index !== -1 ? index + 1: -1;
+  oneDay = oneDay.toLowerCase();
+  let index = days.findIndex((day) => oneDay.startsWith(day));
+  return index + 1 || -1;
 };
 
 module.exports = getDayNumber;
