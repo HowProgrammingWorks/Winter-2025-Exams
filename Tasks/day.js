97b695792aecb826d0fc725cc0fe7fd62c1bc8f6
SanQ1 <88672190+SanQ1@users.noreply.github.com>
2025-01-10 15:37:14 +0200

    day.js - remove loop

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,13 +3,12 @@
 const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (string) => {
-  let result = -1;
-  
-  for (const day of DAYS) {
-    if (string.includes(day)) {
-      result = DAYS.indexOf(day) + 1;
-    }
-  }
+  let result;
+
+  result = DAYS.findIndex((element) => string.includes(element));
+
+  if (result !== -1)
+    ++result;
   
   return result;
 };
