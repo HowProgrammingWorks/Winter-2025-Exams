26e4fc605895c3606e547ad13c17726bb760b4e1
slavareborn <slavaotrishkokpistudy@gmail.com>
2025-01-10 11:34:27 +0200

    Optimize 10 and 11 lines

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -1,18 +1,17 @@
 // Refactor following solution
 // Increment all numbers in dictionary
 
-'use strict'
+'use strict';
 
 const incNumbers = (formatComplete) => {
-  const keys = Object.keys(formatComplete)
+  const keys = Object.keys(formatComplete);
   return keys.reduce((acc, current) => {
-      if (typeof acc[current] === 'number') {
-          acc[current] = formatComplete[current] + 1
-      } else {
-          acc[current] = formatComplete[current]
-      }
-      return acc;
-  }, {})
+    acc[current] = formatComplete[current];
+    if (typeof acc[current] === 'number') {
+      acc[current] + 1;
+    }
+    return acc;
+  }, {});
 };
 
 module.exports = incNumbers;
