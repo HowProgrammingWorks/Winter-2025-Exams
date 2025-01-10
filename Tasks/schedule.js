71a680881b2dc212c240c01ac4b4bade5bc7fa18
Oleksandr Cherepov <sashacherepov55@gmail.com>
2025-01-10 12:20:40 +0200

    Refactor: extract async function 'timeout(interval)'

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -2,10 +2,13 @@
 
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
 
+const timeout = (interval) =>
+  new Promise((resolve) => setInterval(resolve, interval));
+
 const schedule = async (func, n, interval) => {
   for (let i = 0; i < n; i++) {
     func(i);
-    await new Promise((resolve) => setInterval(resolve, interval));
+    await timeout(interval);
   }
 };
 
