59a4bd17b0ed3dd74031957cf1750a5f650be9ed
Mykola Semenenko <mikolasemenenko5@gmail.com>
2025-01-10 14:58:04 +0200

    Decompose into two functions

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,11 +1,13 @@
 // Refactor following solution
 // Get one random element from an array
 
-//Step 3
-//Rewrite the function in one line
+//Step 4
+//Decompose into two functions
 
 'use strict';
 
-const randomElement = (arr) => arr[Math.floor(Math.random() * arr.length)];
+const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
+
+const randomElement = (arr) => arr[getRandomIndex(arr)];
 
 module.exports = randomElement;
