698233fc35a97b73ef3b423038a06905a7153403
MihajloVat <vatolkinmihajlo@gmail.com>
2025-01-10 21:41:26 +0200

    count (step 4 v2)

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,12 +1,11 @@
-// STEP 4:
-//Add ternary operator
-//Rename sum to acc
+// STEP 4 v2:
+//we can use filter instead of ternary operator
 
 const count = (obj) => {
-    const values = Object.values(obj);
-    const res = values.reduce((acc, value) =>
-        typeof value === 'number' ? acc + value : acc, 0);
-    return res;
+  const values = Object.values(obj);
+  const filtered = values.filter((value)=>typeof value === "number");
+  const res = filtered.reduce((acc, value) => acc + value, 0);
+  return res;
 };
 
 module.exports = count;
