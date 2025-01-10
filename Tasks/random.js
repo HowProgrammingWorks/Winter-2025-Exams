721d301ce5d35363c54b22127061aec8307e20d7
Ann0123-cyber <anna.musiiovska@gmail.com>
2025-01-10 15:32:35 +0200

    got rid of unnecessary curly brackets

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,8 +2,6 @@
 // Generate random integer value in given range
 'use strict';
 
-const random = (min, max) => {
-  return min + Math.floor(Math.random() * (max - min + 1));
-};
+const random = (min, max) => min + Math.floor(Math.random() * (max - min + 1));;
 
 module.exports = random;
